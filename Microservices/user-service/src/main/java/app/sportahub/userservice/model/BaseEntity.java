7ce4dec6104346dddf5b87ba151134e5adf4625e
Walid <walidoo_2002@hotmail.com>
2024-11-03 01:44:45 -0400

    GH-32: [feat] Add new user models to support user management

---
+++ Microservices/user-service/src/main/java/app/sportahub/userservice/model/BaseEntity.java
@@ -0,0 +1,24 @@
+package app.sportahub.userservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.sql.Timestamp;
+
+@SuperBuilder(toBuilder = true, setterPrefix = "with")
+@ToString(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class BaseEntity {
+
+    @MongoId(FieldType.OBJECT_ID)
+    private String id;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+}
