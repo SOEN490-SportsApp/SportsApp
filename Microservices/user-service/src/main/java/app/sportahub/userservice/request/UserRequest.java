bd948a6a88c943c86b883e925646c79bf1793b29
Walid <walidoo_2002@hotmail.com>
2024-11-03 01:47:33 -0400

    GH-32: [feat] Add DTOs for user model for request and response handling

---
+++ Microservices/user-service/src/main/java/app/sportahub/userservice/request/UserRequest.java
@@ -0,0 +1,37 @@
+package app.sportahub.userservice.request;
+
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+public record UserRequest(@NotBlank(message = "Email must be provided")
+                          @NotNull(message = "Email must be provided")
+                          @Email(message = "Valid email is required")
+                          String email,
+
+                          @NotBlank(message = "Username must be provided")
+                          @NotNull(message = "Username must be provided")
+                          String username,
+
+                          @NotBlank(message = "Password must be provided")
+                          @NotNull(message = "Password must be provided")
+                          String password,
+
+                          @Nullable
+                          String firstName,
+
+                          @Nullable
+                          String lastName,
+
+                          @Nullable
+                          LocalDate dateOfBirth,
+
+                          @Nullable
+                          String phoneNumber,
+
+                          @Nullable
+                          String ranking) {
+}
