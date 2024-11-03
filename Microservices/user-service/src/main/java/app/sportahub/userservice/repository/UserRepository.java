145ae4160357a6fdb3fbcc71d049386b0bb36ef6
Walid <walidoo_2002@hotmail.com>
2024-11-03 16:05:59 -0500

    GH-32: [feat] Implement UserController, UserRepository, and UserService for user management
    - Added `UserController` to handle incoming HTTP requests for user-related operations.
    - Created `UserRepository` interface for MongoDB interactions with `User` entities.
    - Developed `UserService` and `UserServiceImpl` to encapsulate business logic for user creation, validation, and data persistence.
    
    These additions establish the core layers for user management, enabling API requests to be processed, validated, and persisted in the database.

---
+++ Microservices/user-service/src/main/java/app/sportahub/userservice/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package app.sportahub.userservice.repository;
+
+import app.sportahub.userservice.model.user.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+    User findUserByEmail(String email);
+
+    User findUserByUsername(String username);
+}
---
+++ Microservices/user-service/src/main/java/app/sportahub/userservice/service/user/UserService.java
@@ -0,0 +1,9 @@
+package app.sportahub.userservice.service.user;
+
+import app.sportahub.userservice.model.user.User;
+import app.sportahub.userservice.dto.request.user.UserRequest;
+
+public interface UserService {
+
+    User createUser(UserRequest userRequest);
+}
