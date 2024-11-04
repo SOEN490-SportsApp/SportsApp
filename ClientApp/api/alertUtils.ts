7abf62a194618dffcbf35b0c34b997be8120bb42
JRB958 <141108231+JRB958@users.noreply.github.com>
2024-11-04 11:28:08 -0500

    GH-114: [feat] Create utility function and constants for error logging centralization

---
+++ ClientApp/api/alertUtils.ts
@@ -0,0 +1,41 @@
+/**
+ * This file contains utility functions for showing alerts.
+ * 
+ * This offers the advantage of centralizing alert messages and handling using constants.
+ * showAlert function wraps the "Alert" component from "react-native".
+ * It uses the constants defined in ALERT_MESSAGES to show the alert and allow for easy centralized changes.
+ */
+
+import { Alert } from 'react-native';
+
+//wrapper function for showing alerts
+export const showAlert = (title: string, message: string) => {
+  if (__DEV__) {
+    // allow showing logs in development mode only
+    console.log(`ALERT: ${title} - ${message}`);
+  }
+  Alert.alert(title, message);
+};
+
+// Alert messages for different scenarios
+export const ALERT_MESSAGES = {
+  networkError: {
+    title: 'Network Error',
+    message: 'Please check your connection or server status.',
+  },
+  // for 401 unauthorized status code.
+  sessionExpired: {
+    title: 'Session Expired',
+    message: 'Please log in again.',
+  },
+  // For 500+ server errors, indicating an issue on the server side.
+  serverError: {
+    title: 'Server Error',
+    message: 'Something went wrong. Please try again later.',
+  },
+  // For 404 Not Found.
+  notFound: {
+    title: 'Error',
+    message: 'Requested resource not found.',
+  },
+};
