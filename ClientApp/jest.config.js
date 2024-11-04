9d661727dd6f21f0fd18f8726df5b0e063dfebd3
JRB958 <141108231+JRB958@users.noreply.github.com>
2024-11-04 11:33:32 -0500

    GH-114: [feat] Create an axios instance with centralized error handling and update dependencies

---
+++ ClientApp/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
