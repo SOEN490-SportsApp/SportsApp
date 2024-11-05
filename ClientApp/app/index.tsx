abd50930f2ae123dd6b8778824e579e4d6e6b757
KhalilGr <112202385+KhalilGarali@users.noreply.github.com>
2024-11-04 23:47:49 -0500

    account registration page (#113)
    * GH-31: [feat] Initial routing between login and signup
    
    * GH-31: [feat] Add nativewind config
    
    * GH-31: [feat] Add registration page
    
    * GH-31: [fix] Button component fix
    
    * GH-31: [fix] Removed files from expo app creation
    
    * GH-31: [fix] Remove initial test upon creation of app
    
    * GH-31: [feat] Ui change
    
    * GH-31: [test] Add registration tests

--- ClientApp/app/index.tsx
+++ ClientApp/app/index.tsx
@@ -1,16 +1,5 @@
-import { Text, View } from 'react-native';
+import { Redirect } from 'expo-router';
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Hello guys, this is Sporta, Welcome!</Text>
-      <Text>Lets do thiiisss</Text>
-    </View>
-  );
+  return <Redirect href="/auth/login" />;
 }
