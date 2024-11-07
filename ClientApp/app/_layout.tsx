553afd378682fbc51b4d6c70971678da6746d1d3
NicolasChelico <Nicolas.chelico@outlook.com>
2024-11-06 21:54:32 -0500

    GH-108: [feat] Added progile preference section after creating account

--- ClientApp/app/_layout.tsx
+++ ClientApp/app/_layout.tsx
@@ -1,4 +1,4 @@
-import { Stack } from 'expo-router';
+import { Stack } from "expo-router";
 import "../global.css";
 
 export default function RootLayout() {
@@ -6,6 +6,14 @@ export default function RootLayout() {
     <Stack initialRouteName="login">
       <Stack.Screen name="auth/login" options={{ headerShown: false }} />
       <Stack.Screen name="auth/register" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="auth/profilePreferenceForm"
+        options={{
+          headerShown: true,
+          headerBackTitle: "Back",
+          title: "Profile preference",
+        }}
+      />
     </Stack>
   );
 }
---
+++ ClientApp/util/sports/sportList.ts
@@ -0,0 +1,19 @@
+const sportsArray = [
+  "American Football",
+  "Basketball",
+  "Baseball",
+  "Ice Hockey",
+  "Soccer",
+  "Golf",
+  "Tennis",
+  "Mixed Martial Arts",
+  "Boxing",
+  "Auto Racing",
+  "Lacrosse",
+  "Wrestling",
+  "Cycling",
+  "Rugby",
+  "Horse Racing",
+];
+
+export default sportsArray;
