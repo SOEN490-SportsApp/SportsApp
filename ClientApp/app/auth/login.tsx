b4477e586af22f341f14abc303e425a49728a851
MonikaaMoanes <99571308+MonikaaMoanes@users.noreply.github.com>
2024-11-08 10:59:01 -0500

    GH-110: [feat] Added the home page with navigation bar

--- ClientApp/app/auth/login.tsx
+++ ClientApp/app/auth/login.tsx
@@ -18,7 +18,7 @@ const LoginScreen: React.FC = () => {
 
   const onSubmit = (data: LoginFormData) => {
     console.log("Login data:", data);
-    router.push('/auth/profilePreferenceForm')
+    router.push('/tabs/home');
   };
 
   return (
