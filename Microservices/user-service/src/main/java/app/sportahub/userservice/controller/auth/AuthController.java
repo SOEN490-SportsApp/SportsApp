cf5a7d5d5eebb386f55f5de4b6a5d19381e42fa3
Walid <walidoo_2002@hotmail.com>
2024-11-08 16:35:02 -0500

    GH-32: [refactor] Use Lombok @RequiredArgsConstructor in AuthController for constructor injection

--- Microservices/user-service/src/main/java/app/sportahub/userservice/controller/auth/AuthController.java
+++ Microservices/user-service/src/main/java/app/sportahub/userservice/controller/auth/AuthController.java
@@ -3,18 +3,16 @@ package app.sportahub.userservice.controller.auth;
 import app.sportahub.userservice.dto.request.auth.RegistrationRequest;
 import app.sportahub.userservice.dto.response.user.UserResponse;
 import app.sportahub.userservice.service.auth.AuthService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
     private final AuthService authService;
-
-    public AuthController(AuthService authService) {
-        this.authService = authService;
-    }
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
