daa5a4ceff45d8916848661873f1084c6baf092d
Walid <walidoo_2002@hotmail.com>
2024-11-08 13:37:46 -0500

    GH-32: [refactor] Rename user exception classes for consistency

--- Microservices/user-service/src/main/java/app/sportahub/userservice/exception/user/UsernameAlreadyExistsException.java
+++ Microservices/user-service/src/main/java/app/sportahub/userservice/exception/user/UsernameAlreadyExistsException.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 @ResponseStatus(code = HttpStatus.CONFLICT, reason = "User with this username already exists.")
-public class UsernameAlreadyExists extends ResponseStatusException {
+public class UsernameAlreadyExistsException extends ResponseStatusException {
 
-    public UsernameAlreadyExists(String username) {
+    public UsernameAlreadyExistsException(String username) {
         super(HttpStatus.CONFLICT, "User with this username:" + username + " already exists.");
     }
 }
