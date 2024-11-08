1822e9d527bd78584f491e34f9e2ad0b2a8c12cc
Walid <walidoo_2002@hotmail.com>
2024-11-08 13:31:44 -0500

    GH-32: [chore] Update Gradle build for OAuth2 and Keycloak integration

---
+++ Microservices/user-service/src/main/java/app/sportahub/userservice/config/auth/SecurityConfig.java
@@ -0,0 +1,29 @@
+package app.sportahub.userservice.config.auth;
+
+import lombok.SneakyThrows;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @SneakyThrows
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/auth/register").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .oauth2Login(Customizer.withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))  // JWT token-based auth
+                .build();
+    }
+}
