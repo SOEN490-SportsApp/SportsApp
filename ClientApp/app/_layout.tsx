4a4d8567d3588ae90dfef0bb16c2d92881038118
MonikaaMoanes <99571308+MonikaaMoanes@users.noreply.github.com>
2024-11-09 09:42:39 -0500

    GH-110 : [feat] address reviewer feedback on home page layout

--- ClientApp/app/_layout.tsx
+++ ClientApp/app/_layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout() {
         }}
       />
       <Stack.Screen name="tabs" options={{ headerShown: false }} />
+      <Stack.Screen name="home/landingPage" options={{ headerShown: false }} />
     </Stack>
   );
 }
