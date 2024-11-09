ba0cc96391bde7cccda471d175f7dabea3052e35
Walid <walidoo_2002@hotmail.com>
2024-11-09 17:02:59 -0500

    GH-32: [feat] Add Dockerfile for Spring Boot application deployment

--- Microservices/user-service/src/test/java/app/sportahub/userservice/UserServiceApplicationTests.java
+++ Microservices/user-service/src/test/java/app/sportahub/userservice/UserServiceApplicationTests.java
@@ -3,11 +3,23 @@ package app.sportahub.userservice;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-@TestPropertySource(properties = "spring.data.mongodb.uri=mongodb://root:password@localhost:27017/user-service?authSource=admin")
+@Testcontainers
 public class UserServiceApplicationTests {
+
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
+
+    @DynamicPropertySource
+    static void mongoProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
 
     @Test
     public void contextLoads() {
