4d61ae46f4ddad1352506f0ae8e721fb714cf309
Walid <walidoo_2002@hotmail.com>
2024-11-10 02:24:37 -0500

    GH-32: [feat] Add Swagger to allow requests on remote (#139)
    * GH-32: [chore] Add remote and local server URLs to Swagger UI configuration
    
    * GH-32: [docs] Update Swagger UI tags, descriptions for User and Auth controllers

---
+++ Microservices/user-service/src/main/java/app/sportahub/userservice/config/swagger/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package app.sportahub.userservice.config.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ))
+                .servers(List.of(
+                        new Server().url("https://api-dev.sportahub.app/api/user-service").description("Development"),
+                        new Server().url("http://localhost:8080/api/user-service").description("Local")
+                ));
+    }
+}
--- Microservices/user-service/src/main/java/app/sportahub/userservice/controller/user/UserController.java
+++ Microservices/user-service/src/main/java/app/sportahub/userservice/controller/user/UserController.java
@@ -3,6 +3,8 @@ package app.sportahub.userservice.controller.user;
 import app.sportahub.userservice.dto.request.user.UserRequest;
 import app.sportahub.userservice.dto.response.user.UserResponse;
 import app.sportahub.userservice.service.user.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,18 +13,23 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
+@Tag(name = "User Management", description = "Operations related to user management")
 public class UserController {
 
     private final UserService userService;
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Retrieve user by ID",
+            description = "Fetches a user by their unique identifier.")
     public UserResponse getUserById(@PathVariable String id) {
         return UserResponse.from(userService.getUserById(id));
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Do not use this to register a user! Create a new user",
+            description = "Creates a new user resource to the database based on the provided user details.")
     public UserResponse createUser(@Valid @RequestBody UserRequest userRequest) {
         return UserResponse.from(userService.createUser(userRequest));
     }
