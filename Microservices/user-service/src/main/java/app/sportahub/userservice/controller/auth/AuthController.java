fcfb7359dfc80185d25d6c072e71fdfcb5e9332c
Walid <walidoo_2002@hotmail.com>
2024-11-09 21:42:37 -0500

    GH-32: [docs] Update Swagger UI tags, descriptions for User and Auth controllers

--- Microservices/user-service/src/main/java/app/sportahub/userservice/controller/auth/AuthController.java
+++ Microservices/user-service/src/main/java/app/sportahub/userservice/controller/auth/AuthController.java
@@ -7,6 +7,8 @@ import app.sportahub.userservice.dto.response.auth.LoginResponse;
 import app.sportahub.userservice.dto.response.auth.TokenResponse;
 import app.sportahub.userservice.dto.response.user.UserResponse;
 import app.sportahub.userservice.service.auth.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,24 +16,31 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Tag(name = "Authentication", description = "Operations related to authentication and authorization")
 public class AuthController {
 
     private final AuthService authService;
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Register a new user",
+            description = "Creates a new user account based on the provided registration details.")
     public UserResponse registerUser(@RequestBody RegistrationRequest registrationRequest) {
         return UserResponse.from(authService.registerUser(registrationRequest));
     }
 
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "User login",
+            description = "Authenticates a user and returns an access token and refresh token.")
     public LoginResponse loginUser(@RequestBody LoginRequest loginRequest) {
         return authService.loginUser(loginRequest);
     }
 
     @PostMapping("/refresh")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Refresh access token",
+            description = "Generates a new access token using the provided refresh token.")
     public TokenResponse refreshToken(@RequestBody RefreshTokenRequest tokenRequest) {
         return authService.refreshToken(tokenRequest);
     }
