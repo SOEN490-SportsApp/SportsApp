75edaec50287c1a57830414f8fe37edd3c9dd4b6
wade3hamati <wade3_hamati@outlook.com>
2024-11-10 22:25:47 -0500

    GH-146: [feat] Modify frontend-ci-cd to run linter and check for coverage. Create a backend-ci-cd to be used in future sprints as tests have not been spearated properly yet.

--- git-hooks/add-hooks.sh
+++ git-hooks/add-hooks.sh
@@ -1,31 +1,12 @@
 #!/bin/bash
 
+# Define the source directory for hooks
 HOOKS_DIR="./git-hooks"
-
-if [ ! -d "$HOOKS_DIR" ]; then
-  echo "Error: '$HOOKS_DIR' directory does not exist."
-  exit 1
-fi
-
-if [ ! -f "$HOOKS_DIR/commit-msg" ]; then
-  echo "Error: commit-msg hook file not found in '$HOOKS_DIR'."
-  exit 1
-fi
-
-if [ ! -f "$HOOKS_DIR/pre-push" ]; then
-  echo "Error: pre-push hook file not found in '$HOOKS_DIR'."
-  exit 1
-fi
 
 echo "***** Updating Hooks *****"
 
-echo "Copying commit-msg to .git/hooks/"
 cp -f "$HOOKS_DIR/commit-msg" .git/hooks/commit-msg
 
-echo "Copying pre-push to .git/hooks/"
-cp -f "$HOOKS_DIR/pre-push" .git/hooks/pre-push
-
 chmod ug+x .git/hooks/commit-msg
-chmod ug+x .git/hooks/pre-push
 
 echo "***** Hooks Installed Successfully *****"
