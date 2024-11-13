98d643f9cc12a04bfa3f6f95a6385cbb57455288
Walid <walidoo_2002@hotmail.com>
2024-11-13 16:40:09 -0500

    GH-156: [fix] Activate accounts by default after registration

--- Microservices/user-service/src/main/java/app/sportahub/userservice/dto/request/user/keycloak/KeycloakRequest.java
+++ Microservices/user-service/src/main/java/app/sportahub/userservice/dto/request/user/keycloak/KeycloakRequest.java
@@ -13,12 +13,8 @@ public record KeycloakRequest(
         Boolean enabled,
         List<Credentials> credentials) {
 
-    public KeycloakRequest(String email, String username, String password, String firstName, String lastName) {
-        this(email, username, firstName, lastName, true, List.of(new Credentials(password)));
-    }
-
     public KeycloakRequest(String email, String username, String password) {
-        this(email, username, null, null, null, List.of(new Credentials(password)));
+        this(email, username, null, null, true, List.of(new Credentials(password)));
     }
 
     public KeycloakRequest(String firstName, String lastName) {
