eaac600effd3a8f526da34468d0008854754b079
JRB958 <141108231+JRB958@users.noreply.github.com>
2024-11-18 01:55:55 -0500

    GH-161: [feat] Create a AuthContext that will: handle login logic to be called from the login page handle the logout logic to be called from the logout page contain all the necessary function needed for token managment upon login/logout

--- ClientApp/app/_layout.tsx
+++ ClientApp/app/_layout.tsx
@@ -1,13 +1,17 @@
 import { Stack } from 'expo-router';
+import { AuthProvider } from '../utils/context/AuthContext';
 import "../global.css";
 
 export default function RootLayout() {
   return (
-    <Stack initialRouteName="login">
-      <Stack.Screen name="auth/login" options={{ headerShown: false }} />
-      <Stack.Screen name="auth/registerAccount" options={{ headerShown: false }} />
-      <Stack.Screen name="auth/registerProfile" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <AuthProvider>
+      <Stack initialRouteName="login">
+        <Stack.Screen name="auth/login" options={{ headerShown: false }} />
+        <Stack.Screen name="auth/registerAccount" options={{ headerShown: false }} />
+        <Stack.Screen name="auth/registerProfile" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </AuthProvider>
+    
   );
 }
