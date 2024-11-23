3cc33537773cf1f7b28f67cfbd84f633cd9f6805
yousfino <117748638+yousfino@users.noreply.github.com>
2024-11-23 15:42:32 -0500

    GH-165: [feat] Create: user settings page, accessible from user profile. language preference page, accessible from settings page. notifications preference page, accessible from settings page. log out functionality/button is now implemented in the user settings page.

--- ClientApp/app/_layout.tsx
+++ ClientApp/app/_layout.tsx
@@ -10,6 +10,7 @@ export default function RootLayout() {
         <Stack.Screen name="auth/registerAccount" options={{ headerShown: false }} />
         <Stack.Screen name="auth/registerProfile" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(settings)" options={{ headerShown: false }} />
       </Stack>
     </AuthProvider>
     
