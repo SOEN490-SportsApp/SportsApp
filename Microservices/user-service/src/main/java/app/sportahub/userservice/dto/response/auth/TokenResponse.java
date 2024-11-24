f957cb505d116018eb5d809c635e300c2d0cf9b6
Walid <walidoo_2002@hotmail.com>
2024-11-24 13:51:00 -0500

    GH-152: [feat] Add email verification status on login and on refresh token

--- Microservices/user-service/src/main/java/app/sportahub/userservice/dto/response/auth/TokenResponse.java
+++ Microservices/user-service/src/main/java/app/sportahub/userservice/dto/response/auth/TokenResponse.java
@@ -2,7 +2,9 @@ package app.sportahub.userservice.dto.response.auth;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record TokenResponse(@NotEmpty String accessToken, @NotEmpty String refreshToken) {
+public record TokenResponse(@NotEmpty String accessToken, @NotEmpty String refreshToken,
+                            @NotNull Boolean emailVerified) {
 }
