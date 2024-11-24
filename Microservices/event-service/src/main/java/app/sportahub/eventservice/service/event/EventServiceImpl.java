56e5afc2f1ea26295c18907852d51ac8ad03b2cd
DanDuguay <Duguay9@gmail.com>
2024-11-24 02:46:10 -0500

    GH-103: [refactor] Refactoring code based on PR review

--- Microservices/event-service/src/main/java/app/sportahub/eventservice/service/event/EventServiceImpl.java
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/service/event/EventServiceImpl.java
@@ -3,10 +3,11 @@ package app.sportahub.eventservice.service.event;
 import app.sportahub.eventservice.dto.request.EventRequest;
 import app.sportahub.eventservice.dto.request.ParticipantRequest;
 import app.sportahub.eventservice.dto.request.TeamRequest;
+import app.sportahub.eventservice.dto.response.EventResponse;
 import app.sportahub.eventservice.exception.event.EventAlreadyExistsException;
 import app.sportahub.eventservice.exception.event.EventDoesNotExistException;
+import app.sportahub.eventservice.mapper.event.EventMapper;
 import app.sportahub.eventservice.model.event.Event;
-import app.sportahub.eventservice.model.event.Location;
 import app.sportahub.eventservice.model.event.Participant;
 import app.sportahub.eventservice.model.event.Team;
 import app.sportahub.eventservice.repository.EventRepository;
@@ -18,73 +19,56 @@ import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class EventServiceImpl implements EventService {
+
     private final EventRepository eventRepository;
+    private final EventMapper eventMapper;
 
     @Override
-    public Event getEventById(String id) {
-        return eventRepository.findById(id).orElseThrow(() -> new EventDoesNotExistException(id));
+    public EventResponse getEventById(String id) {
+        return eventMapper.eventToEventResponse(eventRepository.findEventById(id)
+                .orElseThrow(() -> new EventDoesNotExistException(id)));
     }
 
     @Override
-    public Event createEvent(EventRequest eventRequest) {
-        Optional<Event> optionalEventById = eventRepository.findEventById(eventRequest.id());
-
-        if (optionalEventById.isPresent()) {
-            throw new EventAlreadyExistsException(eventRequest.id());
-        }
-
+    public EventResponse createEvent(EventRequest eventRequest) {
+        eventRepository.findEventByEventName(eventRequest.eventName())
+                .ifPresent(event -> {
+                    throw new EventAlreadyExistsException(eventRequest.eventName());
+                });
+        /*
         List<Participant> participants = new ArrayList<>();
 
-        for (ParticipantRequest participantRequest : eventRequest.participants()) {
-            participants.add(Participant.builder().withUserId(participantRequest.userId())
-                    .withAttendStatus(participantRequest.attendStatus())
-                    .withJoinedOn(participantRequest.joinedOn()).build());
+        if (eventRequest.participants() != null) {
+
+            for (ParticipantRequest participantRequest : eventRequest.participants()) {
+                participants.add(Participant.builder().withUserId(participantRequest.userId())
+                        .withAttendStatus(participantRequest.attendStatus())
+                        .withJoinedOn(participantRequest.joinedOn()).build());
+            }
         }
 
         List<Team> teams = new ArrayList<>();
 
-        for (TeamRequest teamRequest : eventRequest.teams()) {
-            teams.add(Team.builder().withTeamId(teamRequest.teamId()).build());
+        if (eventRequest.teams() != null) {
+            for (TeamRequest teamRequest : eventRequest.teams()) {
+                teams.add(Team.builder().withTeamId(teamRequest.teamId()).build());
+            }
         }
+         */
 
-        Event event = Event.builder().withId(eventRequest.id())
+        Event event = eventMapper.eventRequestToEvent(eventRequest)
+                .toBuilder()
                 .withCreationDate(Timestamp.valueOf(LocalDateTime.now()))
                 .withUpdatedAt(Timestamp.valueOf(LocalDateTime.now()))
-                .withEventName(eventRequest.eventName())
-                .withEventType(eventRequest.eventType())
-                .withSportType(eventRequest.sportType())
-                .withLocation(eventRequest.location() != null ? Location.builder()
-                        .withName(eventRequest.location().name())
-                        .withStreetNumber(eventRequest.location().streetNumber())
-                        .withStreetName(eventRequest.location().streetName())
-                        .withCity(eventRequest.location().city())
-                        .withProvince(eventRequest.location().province())
-                        .withCountry(eventRequest.location().country())
-                        .withPostalCode(eventRequest.location().postalCode())
-                        .withAddressLine2(eventRequest.location().adressLine2())
-                        .withPhoneNumber(eventRequest.location().phoneNumber())
-                        .withLatitude(eventRequest.location().latitude())
-                        .withLongitude(eventRequest.location().longitude())
-                                .build()
-                        : null)
-                .withDate(eventRequest.date())
-                .withDuration(eventRequest.duration())
-                .withParticipants(participants)
-                .withCreatedBy(eventRequest.createdBy())
-                .withTeams(teams)
-                .withCutOffTime(eventRequest.cutOffTime())
-                .withDescription(eventRequest.description())
-                .withWhitelistedUsers(eventRequest.whitelistedUsers())
                 .build();
 
         Event savedEvent = eventRepository.save(event);
         log.info("EventServiceImpl::createEvent: Event with id: {} was successfully created", savedEvent.getId());
-        return savedEvent;
+        return eventMapper.eventToEventResponse(savedEvent);
     }
 }
--- Microservices/event-service/src/test/java/app/sportahub/eventservice/EventServiceApplicationTests.java
+++ Microservices/event-service/src/test/java/app/sportahub/eventservice/EventServiceApplicationTests.java
@@ -2,8 +2,9 @@ package app.sportahub.eventservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
-
+@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class EventServiceApplicationTests {
 
