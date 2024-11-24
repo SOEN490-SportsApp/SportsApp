377f6b65f25abc54baacbf50601b4a894c2720f1
Walid <walidoo_2002@hotmail.com>
2024-11-24 13:37:09 -0500

    GH-152: [fix] Disable @Builder for object mappers
    The mapper don't work properly when they use the @Builder annotation, therefore we are not allowing them to use the builder to create the objects.

--- Microservices/user-service/src/main/java/app/sportahub/userservice/mapper/user/UserMapper.java
+++ Microservices/user-service/src/main/java/app/sportahub/userservice/mapper/user/UserMapper.java
@@ -3,9 +3,10 @@ package app.sportahub.userservice.mapper.user;
 import app.sportahub.userservice.dto.request.user.UserRequest;
 import app.sportahub.userservice.dto.response.user.UserResponse;
 import app.sportahub.userservice.model.user.User;
+import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", builder = @Builder(disableBuilder = true))
 public interface UserMapper {
 
     User userRequestToUser(UserRequest userRequest);
