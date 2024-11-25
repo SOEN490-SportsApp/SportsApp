ce07d2cfb039e135bd104ef2da130dceaea8a02f
wade3hamati <wade3_hamati@outlook.com>
2024-11-24 23:32:58 -0500

    GH-155: [feat] Create Bean for ResendConfig.

--- Microservices/email-service/src/main/java/app/sportahub/emailservice/config/ResendConfig.java
+++ Microservices/email-service/src/main/java/app/sportahub/emailservice/config/ResendConfig.java
@@ -1,16 +1,22 @@
 package app.sportahub.emailservice.config;
 
+import com.resend.Resend;
 import lombok.Getter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Getter
 @Configuration
-@Component
 public class ResendConfig {
 
     @Value("${resend.api.key}")
     private String apiKey;
+
+    @Bean
+    public Resend resend() {
+        return new Resend(apiKey);
+    }
 }
 
