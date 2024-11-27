61ca40c91737415c164543447f79d8e99ccfdac6
DanDuguay <Duguay9@gmail.com>
2024-11-27 00:42:04 -0500

    GH-103: [fix] Added unit tests

--- Microservices/event-service/src/main/java/app/sportahub/eventservice/exception/event/EventDoesNotExistException.java
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/exception/event/EventDoesNotExistException.java
@@ -4,10 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
-@ResponseStatus(code = HttpStatus.CONFLICT, reason = "Event does not exist.")
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Event does not exist.")
 public class EventDoesNotExistException extends ResponseStatusException {
 
     public EventDoesNotExistException(String id) {
-        super(HttpStatus.CONFLICT, "Event with id: " + id + " does not exist.");
+        super(HttpStatus.NOT_FOUND, "Event with id: " + id + " does not exist.");
     }
 }
