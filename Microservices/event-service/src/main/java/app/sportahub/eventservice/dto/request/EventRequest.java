81b32fc4c48d73c3cc0d60362c313a2a43835b6f
DanDuguay <Duguay9@gmail.com>
2024-11-26 22:52:02 -0500

    GH-103: Refactoring to add requiredSkillLevel field to events as an EnumSet

--- Microservices/event-service/src/main/java/app/sportahub/eventservice/dto/request/EventRequest.java
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/dto/request/EventRequest.java
@@ -1,49 +1,54 @@
 package app.sportahub.eventservice.dto.request;
 
+import app.sportahub.eventservice.enums.SkillLevelEnum;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
+import java.util.EnumSet;
 import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record EventRequest(
-                           @NotBlank(message = "Event name must be provided")
-                           String eventName,
+        @NotBlank(message = "Event name must be provided")
+        String eventName,
 
-                           @NotBlank(message = "Event type must be provided")
-                           String eventType,
+        @NotBlank(message = "Event type must be provided")
+        String eventType,
 
-                           @NotBlank(message = "Sport type must be provided")
-                           String sportType,
+        @NotBlank(message = "Sport type must be provided")
+        String sportType,
 
-                           @NotNull
-                           LocationRequest location,
+        @NotNull
+        LocationRequest location,
 
-                           @NotNull(message = "Date must be provided")
-                           LocalDate date,
+        @NotNull(message = "Date must be provided")
+        LocalDate date,
 
-                           @Nullable
-                           String duration,
+        @Nullable
+        String duration,
 
-                           @Nullable
-                           List<ParticipantRequest> participants,
+        @Nullable
+        List<ParticipantRequest> participants,
 
-                           @NotBlank(message = "Valid id of the user who created the event must be provided")
-                           String createdBy,
+        @NotBlank(message = "Valid id of the user who created the event must be provided")
+        String createdBy,
 
-                           @Nullable
-                           List<TeamRequest> teams,
+        @Nullable
+        List<TeamRequest> teams,
 
-                           @NotBlank(message = "Cut of time must be provided")
-                           String cutOffTime,
+        @NotBlank(message = "Cut of time must be provided")
+        String cutOffTime,
 
-                           @NotBlank(message = "Description must be provided")
-                           String description,
+        @NotBlank(message = "Description must be provided")
+        String description,
 
-                           @Nullable
-                           List<String> whitelistedUsers){
+        @Nullable
+        List<String> whitelistedUsers,
+
+        @Nullable
+        EnumSet<SkillLevelEnum> requiredSkillLevel){
 
 }
--- Microservices/event-service/src/main/java/app/sportahub/eventservice/dto/response/EventResponse.java
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/dto/response/EventResponse.java
@@ -1,15 +1,17 @@
 package app.sportahub.eventservice.dto.response;
 
+import app.sportahub.eventservice.enums.SkillLevelEnum;
 import app.sportahub.eventservice.model.event.Location;
 import app.sportahub.eventservice.model.event.Participant;
 import app.sportahub.eventservice.model.event.Team;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import java.time.LocalDate;
+import java.util.EnumSet;
 import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record EventResponse(String id, String eventName, String eventType, String sportType, Location location,
                             LocalDate date, String duration, List<Participant> participants, String createdBy,
                             List<Team> teams, String cutOffTime, String description, Boolean isPrivate,
-                            List<String> whitelistedUsers) {
+                            List<String> whitelistedUsers, EnumSet<SkillLevelEnum> requiredSkillLevel) {
 }
