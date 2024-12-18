2b9b637d1a4d1e16f79cf741318b70ec6d59a7cd
Ziadsharkos <ZiadElSharkawi@outlook.com>
2024-12-17 21:42:59 -0500

    GH-77: [chore] Removing unnecessary import

--- Microservices/event-service/src/main/java/app/sportahub/eventservice/controller/EventController.java
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/controller/EventController.java
@@ -10,8 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
-
 @RestController
 @RequestMapping("/event")
 @RequiredArgsConstructor
