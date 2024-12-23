69fef4c13ab2b696f7aa2d026dae8992a37cedeb
wolfie7679 <90850098+wolfie7679@users.noreply.github.com>
2024-12-23 11:29:09 -0500

    GH-180: [feat] Add max participants field to events

--- Microservices/event-service/src/main/java/app/sportahub/eventservice/dto/response/EventResponse.java
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/dto/response/EventResponse.java
@@ -1,18 +1,17 @@
 package app.sportahub.eventservice.dto.response;
 
 import app.sportahub.eventservice.enums.SkillLevelEnum;
-import app.sportahub.eventservice.model.event.Location;
-import app.sportahub.eventservice.model.event.Participant;
+import app.sportahub.eventservice.model.event.participant.Participant;
 import app.sportahub.eventservice.model.event.Team;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import java.time.LocalDate;
 import java.util.EnumSet;
 import java.util.List;
-import java.util.Locale;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record EventResponse(String id, String eventName, String eventType, String sportType, LocationResponse locationResponse,
-                            LocalDate date, String duration, List<Participant> participants, String createdBy,
-                            List<Team> teams, String cutOffTime, String description, Boolean isPrivate,
-                            List<String> whitelistedUsers, EnumSet<SkillLevelEnum> requiredSkillLevel) {
+public record EventResponse(String id, String eventName, String eventType, String sportType,
+                            LocationResponse locationResponse, LocalDate date, String duration, Integer maxParticipants,
+                            List<Participant> participants, String createdBy, List<Team> teams, String cutOffTime,
+                            String description, Boolean isPrivate, List<String> whitelistedUsers,
+                            EnumSet<SkillLevelEnum> requiredSkillLevel) {
 }
