420ebcfb8fb00c022bde970e64ef90b9eede8cd3
Walid <walidoo_2002@hotmail.com>
2024-12-29 14:19:46 -0500

    GH-180: [refactor] Generalize ExceptionHandler to encompass all ResponseStatus Exceptions

--- Microservices/event-service/src/main/java/app/sportahub/eventservice/exception/GlobalExceptionHandler.java
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/exception/GlobalExceptionHandler.java
@@ -1,12 +1,9 @@
 package app.sportahub.eventservice.exception;
 
-import app.sportahub.eventservice.exception.event.EventAlreadyExistsException;
-import app.sportahub.eventservice.exception.event.EventDoesNotExistException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,19 +11,8 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(EventDoesNotExistException.class)
-    public ResponseEntity<Map<String, Object>> handleEventDoesNotExistException(EventDoesNotExistException ex) {
-        Map<String, Object> response = new HashMap<>();
-
-        response.put("error", ex.getReason());
-        response.put("message", ex.getStatusCode().value());
-
-        return ResponseEntity.status(ex.getStatusCode()).body(response);
-    }
-
-    @ExceptionHandler(EventAlreadyExistsException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<Map<String, Object>> handleEventAlreadyExistsException(EventAlreadyExistsException ex) {
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String, Object>> handleEventDoesNotExistException(ResponseStatusException ex) {
         Map<String, Object> response = new HashMap<>();
         response.put("error", ex.getReason());
         response.put("message", ex.getStatusCode().value());
