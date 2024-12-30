7a091fb0e7ba1fb7c1e3cf9ec34ebceaed19d439
yousfino <117748638+yousfino@users.noreply.github.com>
2024-12-29 20:23:05 -0500

    GH-2: [feat] Successful API call to create a new event.

--- ClientApp/services/eventService.ts
+++ ClientApp/services/eventService.ts
@@ -1,9 +1,9 @@
 import axiosInstance from '@/services/axiosInstance';
 import { API_ENDPOINTS } from '@/utils/api/endpoints';
-
+//API_ENDPOINTS.CREATE_EVENT
 export const createEvent = async (eventData: any) => {
   try {
-    const response = await axiosInstance.post(API_ENDPOINTS.CREATE_EVENT, eventData);
+    const response = await axiosInstance.post("https://api-dev.sportahub.app/api/event-service/event", eventData);
     return response.data;
   } catch (error) {
     console.error('Error creating event:', error);
