3593dff23fea7d86ba0822ec28c2825667540f80
DanDuguay <Duguay9@gmail.com>
2024-12-29 23:11:22 -0500

    GH-190: [feat] Rebased and fixed a few things.

--- Microservices/user-service/src/main/java/app/sportahub/userservice/exception/user/UserDoesNotExistException.java
+++ Microservices/user-service/src/main/java/app/sportahub/userservice/exception/user/UserDoesNotExistException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
-@ResponseStatus(code = HttpStatus.CONFLICT, reason = "User does not exist.")
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "User does not exist.")
 public class UserDoesNotExistException extends ResponseStatusException {
 
     public UserDoesNotExistException(String identifier) {
