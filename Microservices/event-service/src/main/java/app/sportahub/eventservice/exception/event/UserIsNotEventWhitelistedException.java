01e73c8e5e1a7338ee268a5e0ecfc947a88efd5c
Walid <walidoo_2002@hotmail.com>
2024-12-29 21:46:41 -0500

    GH-180: [refactor] Change EventRegistrationClosedException and UserIsNotEventWhitelistedException to extend ResponseStatusException class

--- Microservices/event-service/src/main/java/app/sportahub/eventservice/exception/event/UserIsNotEventWhitelistedException.java
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/exception/event/UserIsNotEventWhitelistedException.java
@@ -2,11 +2,13 @@ package app.sportahub.eventservice.exception.event;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @ResponseStatus(code = HttpStatus.FORBIDDEN, reason = "User is not whitelisted for this event.")
-public class UserIsNotEventWhitelistedException extends RuntimeException {
+public class UserIsNotEventWhitelistedException extends ResponseStatusException {
 
     public UserIsNotEventWhitelistedException(String userId, String eventId) {
-        super("User with id: " + userId + " is not whitelisted to join the event with id: " + eventId + ".");
+        super(HttpStatus.FORBIDDEN,
+                "User with id: " + userId + " is not whitelisted to join the event with id: " + eventId + ".");
     }
 }
