666b20e09dc92cdb91a27debeda83e05f4e87df8
Walid <walidoo_2002@hotmail.com>
2024-12-29 21:46:41 -0500

    GH-180: [refactor] Change EventRegistrationClosedException and UserIsNotEventWhitelistedException to extend ResponseStatusException class

--- Microservices/event-service/src/main/java/app/sportahub/eventservice/exception/event/EventRegistrationClosedException.java
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/exception/event/EventRegistrationClosedException.java
@@ -2,11 +2,12 @@ package app.sportahub.eventservice.exception.event;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @ResponseStatus(code = HttpStatus.FORBIDDEN, reason = "Event is closed for registration.")
-public class EventRegistrationClosedException extends RuntimeException {
+public class EventRegistrationClosedException extends ResponseStatusException {
 
     public EventRegistrationClosedException(String eventId) {
-        super("The event with id: " + eventId + " is closed for registration.");
+        super(HttpStatus.FORBIDDEN, "The event with id: " + eventId + " is closed for registration.");
     }
 }
