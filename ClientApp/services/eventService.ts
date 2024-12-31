c43b805aa855796e562d72d48251a0cd66723028
yousfino <117748638+yousfino@users.noreply.github.com>
2024-12-30 22:52:32 -0500

    GH-2: [refactor] CR refactored specific parts of the code - khalil's comments.

--- ClientApp/services/eventService.ts
+++ ClientApp/services/eventService.ts
@@ -3,7 +3,7 @@ import { API_ENDPOINTS } from '@/utils/api/endpoints';
 //API_ENDPOINTS.CREATE_EVENT
 export const createEvent = async (eventData: any) => {
   try {
-    const response = await axiosInstance.post("https://api-dev.sportahub.app/api/event-service/event", eventData);
+    const response = await axiosInstance.post(API_ENDPOINTS.CREATE_EVENT, eventData);
     return response.data;
   } catch (error) {
     console.error('Error creating event:', error);
