c3102b6c38b44e4ba17498f294551f6d5b29fa94
NicolasChelico <Nicolas.chelico@outlook.com>
2025-01-10 22:58:17 -0500

    GH-217: [refactor] Refactored the create account routing + fixed failing test cases

--- ClientApp/__tests__/services/authService.test.ts
+++ ClientApp/__tests__/services/authService.test.ts
@@ -1,13 +1,18 @@
-import axiosInstance from "@/services/axiosInstance";
+import { getAxiosInstance } from "@/services/axiosInstance";
 import { clearTokens, saveTokens, startTokenRefresh, stopTokenRefresh } from "@/services/tokenService";
 import { loginUser, logoutUser } from "@/services/authService";
 import { useDispatch } from 'react-redux';
 import { clearUser } from "@/state/user/userSlice";
 
 
-jest.mock('@/services/axiosInstance', () => ({
-    post: jest.fn(),
-}));
+jest.mock('@/services/axiosInstance', () => {
+    const mockAxiosInstance = {
+        post: jest.fn(),
+    };
+    return {
+        getAxiosInstance: jest.fn(() => mockAxiosInstance),
+    };
+});
 
 jest.mock('@/services/tokenService', () => ({
     clearTokens: jest.fn(),
@@ -21,6 +26,13 @@ jest.mock('react-redux', () => ({
   }));
 
 describe('Auth Service Tests', () => {
+    let axiosInstance: any;
+
+    beforeAll(() => {
+        axiosInstance = getAxiosInstance();
+    });
+
+
     afterEach(() => {
         jest.clearAllMocks();
     });
@@ -44,7 +56,7 @@ describe('Auth Service Tests', () => {
     });
 
     it('should throw error on login failure', async () => {
-        (axiosInstance.post as jest.Mock).mockRejectedValueOnce(new Error('Login failed'));
+        (getAxiosInstance().post as jest.Mock).mockRejectedValueOnce(new Error('Login failed'));
 
         await expect(loginUser('username', 'password')).rejects.toThrow('Login failed');
         expect(saveTokens).not.toHaveBeenCalled();
@@ -57,7 +69,7 @@ describe('Auth Service Tests', () => {
         (clearTokens as jest.Mock).mockResolvedValueOnce(true); // Simulate successful clear
         (stopTokenRefresh as jest.Mock).mockResolvedValueOnce(null); // Simulate successful stop
 
-        await logoutUser();
+        await logoutUser(mockDispatch);
 
         expect(clearTokens).toHaveBeenCalledTimes(1);
         expect(stopTokenRefresh).toHaveBeenCalledTimes(1);
