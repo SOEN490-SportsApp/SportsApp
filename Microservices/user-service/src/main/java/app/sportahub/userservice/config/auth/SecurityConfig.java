d1a7e90b053d4e73762513d902c961f03fb8da23
patrickmac3 <patrickmaceachen9@gmail.com>
2025-01-12 01:44:14 -0500

    GH-181: [feat] Authorization for user service

--- Microservices/user-service/src/main/java/app/sportahub/userservice/config/auth/SecurityConfig.java
+++ Microservices/user-service/src/main/java/app/sportahub/userservice/config/auth/SecurityConfig.java
@@ -1,23 +1,34 @@
 package app.sportahub.userservice.config.auth;
 
-import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import app.sportahub.userservice.utils.KeycloakJwtAuthenticationConverter;
+import lombok.SneakyThrows;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        return KeycloakJwtAuthenticationConverter.jwtAuthenticationConverter();
+    }
 
     @SneakyThrows
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         return http
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+
                         .requestMatchers(
                                 "/auth/register",
                                 "/auth/login",
@@ -27,13 +38,15 @@ public class SecurityConfig {
                                 "/api-docs",
                                 "/api-docs/**",
                                 "/swagger-ui/**",
-                                "/webjars/**"
-                        ).permitAll()
-                        .anyRequest().authenticated()
-                )
+                                "/webjars/**")
+                        .permitAll()
+                        .anyRequest().authenticated())
                 .csrf(AbstractHttpConfigurer::disable)
                 .oauth2Login(Customizer.withDefaults())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))  // JWT token-based auth
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(
+                                        jwtAuthenticationConverter())))
                 .build();
     }
 }
