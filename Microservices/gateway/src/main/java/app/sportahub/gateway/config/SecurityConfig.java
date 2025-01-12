fbc9090d60a60c8ff5c577598ea81766fde19414
patrickmac3 <patrickmaceachen9@gmail.com>
2025-01-12 01:47:30 -0500

    GH-181: [feat] Authorization for api gateway

---
+++ Microservices/gateway/src/main/java/app/sportahub/gateway/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package app.sportahub.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.SneakyThrows;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @SneakyThrows
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers(
+                                "/api/user-service/auth/register",
+                                "/api/user-service/auth/login",
+                                "/api/user-service/auth/refresh",
+                                "/api/user-service/auth/reset-password")
+                        .permitAll()
+                        .anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .oauth2Login(Customizer.withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())) // JWT token-based auth
+                .build();
+    }
+}
