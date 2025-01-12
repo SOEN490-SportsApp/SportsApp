7ae246657b1cd8691163429d000a2ea21d8a334c
patrickmac3 <patrickmaceachen9@gmail.com>
2025-01-12 01:47:30 -0500

    GH-181: [feat] Authorization for api gateway

--- Microservices/gateway/src/test/java/app/sportahub/gateway/GatewayApplicationTests.java
+++ Microservices/gateway/src/test/java/app/sportahub/gateway/GatewayApplicationTests.java
@@ -1,13 +1,15 @@
 package app.sportahub.gateway;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true, "Application context loaded successfully");
+    }
 
 }
