8250ad16830e501f17e12782ceb17d0a769e807e
wade3hamati <wade3_hamati@outlook.com>
2025-01-14 11:21:26 -0500

    GH-214: [feat] Add ObservationConfig for micrometer observation support.

---
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/config/ObservationConfig.java
@@ -0,0 +1,13 @@
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObservationConfig {
+
+    @Bean
+    ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
+        return new ObservedAspect(observationRegistry);
+    }
+}
