519370fe3d3b0c9a3834ecb83c5e8f23cc408210
DanDuguay <Duguay9@gmail.com>
2025-01-15 15:11:48 -0500

    GH-210: [refactor] Added requestId and status to the ViewFriendRequestsResponse

--- Microservices/user-service/src/main/java/app/sportahub/userservice/dto/response/user/friend/ViewFriendRequestsResponse.java
+++ Microservices/user-service/src/main/java/app/sportahub/userservice/dto/response/user/friend/ViewFriendRequestsResponse.java
@@ -1,4 +1,6 @@
 package app.sportahub.userservice.dto.response.user.friend;
 
-public record ViewFriendRequestsResponse(String friendUsername, String friendUserId, String id) {
+import app.sportahub.userservice.enums.user.FriendRequestStatusEnum;
+
+public record ViewFriendRequestsResponse(String friendUsername, String friendUserId, FriendRequestStatusEnum status, String id) {
 }
