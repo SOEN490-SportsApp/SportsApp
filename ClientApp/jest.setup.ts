1190642965f769f19fb14fb553b83fcc2d5b2f51
NicolasChelico <Nicolas.chelico@outlook.com>
2025-01-17 13:01:48 -0500

    GH-249: [test] Fix failing CI tests

--- ClientApp/jest.setup.ts
+++ ClientApp/jest.setup.ts
@@ -1,3 +1,7 @@
 // jestSetup.ts
 
 jest.mock("expo-font");
+
+// jest.setup.js
+global.setImmediate = global.setImmediate || ((fn: (...args: any[]) => void, ...args: any) => setTimeout(fn, 0, ...args));
+global.clearImmediate = global.clearImmediate || clearTimeout;
