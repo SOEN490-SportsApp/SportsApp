d88cbddb6858a9609abbbecacfa4ed27c5273f2b
MonikaaMoanes <99571308+MonikaaMoanes@users.noreply.github.com>
2025-01-24 18:45:29 -0500

    GH-237: [fix] Refine error handling and remove console errors from the app

--- ClientApp/services/authService.ts
+++ ClientApp/services/authService.ts
@@ -1,7 +1,6 @@
 import { saveTokens, clearTokens, startTokenRefresh, stopTokenRefresh } from './tokenService';
 import { API_ENDPOINTS } from '@/utils/api/endpoints';
 import { clearUser } from '@/state/user/userSlice';
-import { useDispatch } from 'react-redux';
 import { getAxiosInstance } from './axiosInstance';
 import { ALERT_MESSAGES } from '@/utils/api/errorHandlers';
 
@@ -16,12 +15,9 @@ export async function loginUser(identifier: string, password: string) {
   } catch (error: any) {
       const serverError = error.response?.data?.error;
 
-      // Check for invalid credentials and throw a user-friendly error
-      if (serverError?.includes("invalid_grant")) {
+      if (typeof serverError === "string" && serverError.includes("invalid_grant")) {
           throw new Error(ALERT_MESSAGES.invalidCredentials.message);
       }
-
-      console.error('Login failed:', error);
       throw error.response?.data || error;
   }
 }
@@ -32,7 +28,9 @@ export async function logoutUser(dispatch: any) {
     dispatch(clearUser());
     stopTokenRefresh();
   } catch (error: any) {
-    console.error('Error Loging out user:', error);
+    if (__DEV__) {
+        console.error('Error Logging out user:', error.message || error);
+    }
     throw error.response?.data || error;
   }
 }
@@ -40,21 +38,25 @@ export async function logoutUser(dispatch: any) {
 export async function registerUser(email: string, username: string, password: string) {
   const axiosInstance = getAxiosInstance();
   try {
-    const response = await axiosInstance.post(API_ENDPOINTS.REGISTER, {email, username, password});
+    const response = await axiosInstance.post(API_ENDPOINTS.REGISTER, { email, username, password });
     return response;
   } catch (error: any) {
-    console.error("Registration failed:", error);    
+    if (__DEV__) {
+        console.error("Registration failed:", error.message || error);
+    }
     throw error.response?.data || error;
   }
 }
 
-export async function resetPassword(email: string){
+export async function resetPassword(email: string) {
   const axiosInstance = getAxiosInstance();
-  try{
-    const response = await axiosInstance.put(API_ENDPOINTS.RESET_PASSWORD, {email})
+  try {
+    const response = await axiosInstance.put(API_ENDPOINTS.RESET_PASSWORD, { email });
     return response;
-  }catch(err: any){
-    console.log("Reset password failed: ", err)
+  } catch (err: any) {
+    if (__DEV__) {
+        console.error("Reset password failed:", err.message || err);
+    }
     throw err.response?.data || err;
   }
 }
