73835c6cb43633640b7e97f7295639ac83b20df1
wade3hamati <wade3_hamati@outlook.com>
2025-02-03 23:53:28 -0500

    GH-298: [feat] Use JavaMailSender and Thymeleaf to send email.

--- Microservices/email-service/src/main/java/app/sportahub/emailservice/service/EmailServiceImpl.java
+++ Microservices/email-service/src/main/java/app/sportahub/emailservice/service/EmailServiceImpl.java
@@ -1,60 +1,44 @@
 package app.sportahub.emailservice.service;
-
-import app.sportahub.emailservice.dto.request.EmailRequest;
-import app.sportahub.emailservice.event.EmailSentEvent;
-import com.resend.*;
-import com.resend.services.emails.model.CreateEmailOptions;
-import com.resend.services.emails.model.CreateEmailResponse;
+import jakarta.mail.*;
+import jakarta.mail.internet.*;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.messaging.MessagingException;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
 
-    private final Resend resend;
-
-
-    @KafkaListener(topics = "email-sent")
-    public void listen(EmailSentEvent emailSentEvent) {
-        try{
-            log.info("Received email sent: {}", emailSentEvent);
-            EmailRequest emailRequest = new EmailRequest();
-            emailRequest.setRecipientEmailAddress(emailSentEvent.getRecipientEmailAddress());
-            emailRequest.setSenderEmailAddress(emailSentEvent.getSenderEmailAddress());
-            emailRequest.setSubject(emailSentEvent.getSubject());
-            emailRequest.setBody(emailSentEvent.getBody());
-            sendEmail(emailRequest);
-        }
-        catch (Exception e) {
-
-        }
-    }
+    private final JavaMailSender mailSender;
+    private final TemplateEngine templateEngine;
+    @Value("${spring.mail.username}")
+    private String fromEmail;
 
     @Override
-    @SneakyThrows
-    public void sendEmail(EmailRequest emailRequest) {
-        log.info("EmailServiceImpl::sendEmail: Attempting to send email to {}", emailRequest.getRecipientEmailAddress());
+    public void sendForgotPasswordEmail(String email) throws MessagingException, jakarta.mail.MessagingException {
+        Context context = new Context();
+        String htmlContent = templateEngine.process("email/password-reset", context);
 
-        String from = emailRequest.getSenderEmailAddress();
-        String to = emailRequest.getRecipientEmailAddress();
-        String subject = emailRequest.getSubject();
-        String body = emailRequest.getBody();
-        String htmlContent = emailRequest.getHtmlContent();
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, StandardCharsets.UTF_8.name());
 
-        CreateEmailOptions params = CreateEmailOptions.builder()
-                .from(from)
-                .to(to)
-                .subject(subject)
-                .html(htmlContent)
-                .build();
-
-        CreateEmailResponse response = resend.emails().send(params);
-        log.info("Email sent successfully with ID: {}", response.getId());
+        helper.setFrom(fromEmail);
+        helper.setTo(email);
+        helper.setSubject("Password Reset");
+        helper.setText(htmlContent, true);
+        mailSender.send(message);
+        log.info("EmailServiceImpl::sendForgotPasswordEmail: Forgot Password Reset Email Sent to {}", email);
     }
 }
 
