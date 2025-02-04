afc2a75f56812a1cbb90eddae542176f25d08aa8
wade3hamati <wade3_hamati@outlook.com>
2025-02-03 22:28:01 -0500

    GH-298: [feat] Create Orchestration microservice to act as a central controller of kafka messaging between other microservices.

--- Microservices/orchestration-service/src/test/java/app/sportahub/orchestrationservice/TestcontainersConfiguration.java
+++ Microservices/orchestration-service/src/test/java/app/sportahub/orchestrationservice/TestcontainersConfiguration.java
@@ -1,18 +1,18 @@
-package app.sportahub.emailservice;
+package app.sportahub.orchestrationservice;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
-	@Bean
-	@ServiceConnection
-	MongoDBContainer mongoDbContainer() {
-		return new MongoDBContainer(DockerImageName.parse("mongo:latest"));
-	}
+    @Bean
+    @ServiceConnection
+    KafkaContainer kafkaContainer() {
+        return new KafkaContainer(DockerImageName.parse("apache/kafka-native:latest"));
+    }
 
 }
