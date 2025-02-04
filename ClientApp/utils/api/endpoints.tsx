d830ae29aa967b4319ba0832c6d42c14e5beb3a1
KhalilGr <garalikhalil@gmail.com>
2025-02-04 00:28:05 -0500

    GH-284: [feat] Add logic to user profile visit

--- ClientApp/utils/api/endpoints.tsx
+++ ClientApp/utils/api/endpoints.tsx
@@ -6,6 +6,8 @@ export const API_ENDPOINTS = {
   RESET_PASSWORD: "user-service/auth/reset-password",
   GET_USER_BY_ID: "user-service/user/{id}",
   SEARCH_USERS: 'user-service/user/search',
+  GET_USER_PROFILE: 'user-service/user/{id}/profile',
+
   CREATE_EVENT: "event-service/event",
   GET_EVENT_BY_ID: "event-service/event/{id}",
   GET_ALL_EVENTS: "event-service/event",
@@ -14,4 +16,5 @@ export const API_ENDPOINTS = {
   GET_ALL_EVENTS_JOINED: 'event-service/event/patricipant/{userId}', //typo in backend endpoint, shouldn't really need a fix
   GET_ALL_EVENTS_CREATED_BY: 'event-service/event/created-by/{userId}',
     DELETE_PROFILE: 'user-service/user/{userId}',
+  GET_EVETNS_BY_USER_ID: "event-service/event/patricipant/{userId}",
 };
