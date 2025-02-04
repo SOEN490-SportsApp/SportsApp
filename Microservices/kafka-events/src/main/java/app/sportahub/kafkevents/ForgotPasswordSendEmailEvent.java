e2c43639c2e8ed41473d9ba898473fb94a070271
wade3hamati <wade3_hamati@outlook.com>
2025-02-03 23:37:03 -0500

    GH-298: [feat] Create ForgotPasswordRequestedEvent to capture user-orchestrator communication and ForgotPasswordSendEmailEvent to capture orchestrator-email communication.

---
+++ Microservices/kafka-events/src/main/java/app/sportahub/kafkevents/ForgotPasswordSendEmailEvent.java
@@ -0,0 +1,20 @@
+package app.sportahub.kafkevents;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ForgotPasswordSendEmailEvent {
+    private BaseEvent baseEvent;
+    private String email;
+
+    @JsonCreator
+    public ForgotPasswordSendEmailEvent(
+            @JsonProperty("baseEvent") BaseEvent baseEvent,
+            @JsonProperty("email") String email)
+    {
+        this.baseEvent = baseEvent;
+        this.email = email;
+    }
+    public String getEmail() {return this.email;}
+    public BaseEvent getBaseEvent() {return this.baseEvent;}
+}
