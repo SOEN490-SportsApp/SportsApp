3c531e9f89458bcf7b55c36edeeda3a1f423db8a
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-02-06 17:56:36 -0500

    GH-288: [chore] Add MinioConfig to use a minioClient for bucket interactions

---
+++ Microservices/storage-service/src/main/java/app/sportahub/storageservice/config/MinioConfig.java
@@ -0,0 +1,30 @@
+package app.sportahub.storageservice.config;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioConfig {
+    @Value("${minio.url}")
+    private String minioUrl;
+
+    @Value("${minio.access.name}")
+    private String accessKey;
+
+    @Value("${minio.access.secret}")
+    private String secretKey;
+
+    @Value("${minio.bucket.name}")
+    private String bucketName;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(minioUrl)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+}
+
