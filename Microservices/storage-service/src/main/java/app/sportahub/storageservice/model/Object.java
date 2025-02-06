543743ee4c161824b740a527e622356f75c3aa77
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-02-06 17:41:45 -0500

    GH-288: [feat] Add new Object model to support object management

---
+++ Microservices/storage-service/src/main/java/app/sportahub/storageservice/model/Object.java
@@ -0,0 +1,30 @@
+package app.sportahub.storageservice.model;
+
+import lombok.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.experimental.SuperBuilder;
+
+import java.io.InputStream;
+import java.time.LocalDateTime;
+
+@EqualsAndHashCode(callSuper=false)
+@SuperBuilder(toBuilder = true, setterPrefix = "with")
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+public class Object {
+    @NotBlank(message = "File name must be provided")
+    private String fileName;
+
+    @NotBlank(message = "Content type must be provided")
+    private String contentType;
+
+    private long size;
+    private InputStream contentStream;
+    private String downloadUrl;
+
+    @Builder.Default
+    private LocalDateTime uploadTime = LocalDateTime.now();
+
+}
