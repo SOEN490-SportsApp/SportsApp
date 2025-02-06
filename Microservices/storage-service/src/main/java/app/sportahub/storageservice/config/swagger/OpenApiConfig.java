722c105b5c75fb91cb897e3ee54a23e98af931e0
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-02-06 18:44:42 -0500

    GH-288: [chore] Change URL to storage service URL

--- Microservices/storage-service/src/main/java/app/sportahub/storageservice/config/swagger/OpenApiConfig.java
+++ Microservices/storage-service/src/main/java/app/sportahub/storageservice/config/swagger/OpenApiConfig.java
@@ -24,8 +24,8 @@ public class OpenApiConfig {
                                         .bearerFormat("JWT")
                         ))
                 .servers(List.of(
-                        new Server().url("https://api-dev.sportahub.app/api/user-service").description("Development"),
-                        new Server().url("http://localhost:8080/api/user-service").description("Local")
+                        new Server().url("https://api-dev.sportahub.app/api/storage-service").description("Development"),
+                        new Server().url("http://localhost:8080/api/storage-service").description("Local")
                 ));
     }
 }
