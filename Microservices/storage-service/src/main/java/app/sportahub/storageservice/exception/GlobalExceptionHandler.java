8cb7ea88f0f96db895adbe04349f0e57dc86565a
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-02-06 21:46:30 -0500

    GH-288: [chore] Add GlobalExceptionHandler and exception for null files

---
+++ Microservices/storage-service/src/main/java/app/sportahub/storageservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package app.sportahub.storageservice.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String, Object>> handleResponseStatusException(ResponseStatusException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("error", ex.getReason());
+        response.put("message", ex.getStatusCode().value());
+        return ResponseEntity.status(ex.getStatusCode()).body(response);
+    }
+}
