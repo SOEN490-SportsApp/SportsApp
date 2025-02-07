646531a591da03b0978554e200147102e05893c4
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-02-06 19:47:48 -0500

    GH-288: [chore] Remove Unnecessary StorageException

--- Microservices/storage-service/src/main/java/app/sportahub/storageservice/exception/StorageException.java
+++
@@ -1,7 +0,0 @@
-package app.sportahub.storageservice.exception;
-
-public class StorageException extends RuntimeException{
-    public StorageException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
