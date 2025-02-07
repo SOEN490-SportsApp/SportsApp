2bf1237ed0f37b03076f8b057b5ee872958a0148
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-02-06 22:02:25 -0500

    GH-288: [chore] Remove unnecessary files

--- Microservices/storage-service/src/main/java/app/sportahub/storageservice/service/ObjectService.java
+++
@@ -1,9 +0,0 @@
-package app.sportahub.storageservice.service;
-
-import app.sportahub.storageservice.model.Object;
-import org.springframework.web.multipart.MultipartFile;
-
-public interface ObjectService {
-
-    Object storeFile(MultipartFile file);
-}
