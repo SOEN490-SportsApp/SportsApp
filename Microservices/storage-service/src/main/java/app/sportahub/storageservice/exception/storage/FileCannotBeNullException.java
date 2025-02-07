a00b4bdc64c0af7fb7f65a5dec5e681c661d7f78
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-02-06 22:01:40 -0500

    GH-288: [chore] Add exception for null files

--- Microservices/storage-service/src/main/java/app/sportahub/storageservice/exception/storage/FileCannotBeNullException.java
+++ Microservices/storage-service/src/main/java/app/sportahub/storageservice/exception/storage/FileCannotBeNullException.java
@@ -1,7 +1,6 @@
 package app.sportahub.storageservice.exception.storage;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
