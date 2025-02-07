7d12be210a96f591c4bfe75f94a582ce46e66535
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-02-06 19:50:05 -0500

    GH-288: [chore] Removing Unnecessary methods in interface

--- Microservices/storage-service/src/main/java/app/sportahub/storageservice/service/ObjectService.java
+++ Microservices/storage-service/src/main/java/app/sportahub/storageservice/service/ObjectService.java
@@ -2,15 +2,8 @@ package app.sportahub.storageservice.service;
 
 import app.sportahub.storageservice.model.Object;
 import org.springframework.web.multipart.MultipartFile;
-import java.util.List;
 
 public interface ObjectService {
 
     Object storeFile(MultipartFile file);
-
-    Object retrieveFile(String fileName);
-
-    void deleteFile(String fileName);
-
-    List<Object> listAllFiles();
 }
