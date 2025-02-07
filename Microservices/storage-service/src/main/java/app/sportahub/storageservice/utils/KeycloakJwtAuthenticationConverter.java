3a97812a9536b64c501dc8a6d842ef64c9ab858e
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-02-06 19:52:21 -0500

    GH-288: [chore] Optimize imports

--- Microservices/storage-service/src/main/java/app/sportahub/storageservice/utils/KeycloakJwtAuthenticationConverter.java
+++ Microservices/storage-service/src/main/java/app/sportahub/storageservice/utils/KeycloakJwtAuthenticationConverter.java
@@ -1,16 +1,16 @@
 package app.sportahub.storageservice.utils;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class KeycloakJwtAuthenticationConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
 
