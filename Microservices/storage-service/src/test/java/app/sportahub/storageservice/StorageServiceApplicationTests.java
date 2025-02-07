d55ef552b5726f19ec233b31e27a56cc7c99af76
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-02-06 22:02:25 -0500

    GH-288: [chore] Remove unnecessary files

--- Microservices/storage-service/src/test/java/app/sportahub/storageservice/StorageServiceApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package app.sportahub.storageservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class StorageServiceApplicationTests {
-
-}
