7a645229b279ae0f27e0b7bd0f9dd10767d13e16
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-02-06 19:51:39 -0500

    GH-288: [chore] Remove unnecessary download method and returning ObjectResponse instead of response entity

--- Microservices/storage-service/src/main/java/app/sportahub/storageservice/controller/ObjectController.java
+++ Microservices/storage-service/src/main/java/app/sportahub/storageservice/controller/ObjectController.java
@@ -1,18 +1,16 @@
 package app.sportahub.storageservice.controller;
 
+import app.sportahub.storageservice.dto.response.ObjectResponse;
 import app.sportahub.storageservice.model.Object;
 import app.sportahub.storageservice.service.ObjectService;
 import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.core.io.InputStreamResource;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/objects")
@@ -24,40 +22,13 @@ public class ObjectController {
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(summary = "Creates a new event",
             description = "Creates a new event resource to the database based on the provided event details.")
-    public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile file) {
+    public ObjectResponse uploadFile(@RequestParam("file") MultipartFile file) {
         Object objectInfo = objectService.storeFile(file);
         String downloadUrl = ServletUriComponentsBuilder.fromCurrentContextPath()
                 .path("/objects/download/")
                 .path(objectInfo.getFileName())
                 .toUriString();
 
-        objectInfo.setDownloadUrl(downloadUrl);
-        return ResponseEntity.ok(objectInfo);
-    }
-
-    @GetMapping("/download/{fileName}")
-    @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "Download Object using URL",
-            description = "Downloads the desired object using the URL returned when uploading object.")
-    public ResponseEntity<InputStreamResource> downloadFile(@PathVariable String fileName) {
-        Object objectInfo = objectService.retrieveFile(fileName);
-        if (objectInfo.getContentStream() == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(objectInfo.getContentType()))
-                .body(new InputStreamResource(objectInfo.getContentStream()));
-    }
-
-    @DeleteMapping("/{fileName}")
-    public ResponseEntity<Void> deleteFile(@PathVariable String fileName) {
-        objectService.deleteFile(fileName);
-        return ResponseEntity.ok().build();
-    }
-
-    @GetMapping
-    public ResponseEntity<List<Object>> listAllFiles() {
-        List<Object> objects = objectService.listAllFiles();
-        return ResponseEntity.ok(objects);
+        return new ObjectResponse(file.getName(), file.getContentType(), file.getSize(), LocalDateTime.now(), downloadUrl);
     }
 }
