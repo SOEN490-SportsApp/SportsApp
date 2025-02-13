b8eb5629dfd455ce535727887c291c757a3234a9
wade3hamati <wade3_hamati@outlook.com>
2025-02-13 11:31:47 -0500

    GH-298: [feat] Write unit tests.

---
+++ Microservices/email-service/src/test/java/app/sportahub/emailservice/service/kafka/consumer/OrchestrationServiceTest.java
@@ -0,0 +1,46 @@
+package app.sportahub.emailservice.service.kafka.consumer;
+
+import app.sportahub.ForgotPasswordSendEmailEvent;
+import app.sportahub.BaseEvent;
+import app.sportahub.emailservice.service.EmailService;
+import jakarta.mail.MessagingException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class OrchestrationServiceTest {
+    @Mock
+    private EmailService emailService;
+
+    @InjectMocks
+    private OrchestrationServiceConsumerImpl orchestrationService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testListenForForgotPasswordValidateEvent() throws MessagingException {
+        String email = "test@example.com";
+        BaseEvent baseEvent = new BaseEvent(
+                UUID.randomUUID().toString(),
+                "request",
+                "user-service",
+                Instant.now(),
+                UUID.randomUUID().toString());
+        ForgotPasswordSendEmailEvent event = new ForgotPasswordSendEmailEvent(baseEvent, email);
+
+        orchestrationService.listenForForgotPasswordValidateEvent(event);
+
+        verify(emailService, times(1)).sendForgotPasswordEmail(event.getEmail());
+    }
+}
