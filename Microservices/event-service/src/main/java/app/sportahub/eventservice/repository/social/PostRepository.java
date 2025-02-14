b710f8d370b9fed3d4cbf35b06dec9b910c3f92d
Walid <walidoo_2002@hotmail.com>
2025-02-14 17:31:40 -0500

    GH-323: [feat] Add endpoint to retrieve posts associated to an event in order of earliest to latest

--- Microservices/event-service/src/main/java/app/sportahub/eventservice/repository/social/PostRepository.java
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/repository/social/PostRepository.java
@@ -1,9 +1,12 @@
 package app.sportahub.eventservice.repository.social;
 
 import app.sportahub.eventservice.model.social.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+    Page<Post> findByEventId(String eventId, Pageable pageable);
 }
