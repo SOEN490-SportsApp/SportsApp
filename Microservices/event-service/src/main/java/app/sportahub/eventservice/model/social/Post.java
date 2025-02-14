e3dec91c0580ac06a078a614050a7d6e4e776063
Walid <walidoo_2002@hotmail.com>
2025-02-13 23:25:06 -0500

    GH-323: [feat] Add post, comment models to event model

---
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/model/social/Post.java
@@ -0,0 +1,37 @@
+package app.sportahub.eventservice.model.social;
+
+import app.sportahub.eventservice.model.BaseEntity;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = false)
+@SuperBuilder(toBuilder = true, setterPrefix = "with")
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "posts")
+@ToString
+@Data
+public class Post extends BaseEntity {
+
+    @NotBlank(message = "Valid id of the event must be provided")
+    private String eventId;
+
+    @NotBlank(message = "Post content must be provided")
+    private String content;
+
+    @NotBlank(message = "Valid id of the user who created the post must be provided")
+    private String createdBy;
+
+    @Builder.Default
+    private List<String> attachments = new ArrayList<>();
+
+    @DBRef(lazy = true)
+    @Builder.Default
+    private List<Comment> comments = new ArrayList<>();
+}
