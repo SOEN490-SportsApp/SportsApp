82ced835d59771a2e50d60b68a74a84deb5c5f7d
Walid <walidoo_2002@hotmail.com>
2025-02-14 16:24:13 -0500

    GH-323: [feat] Add a comment mapper

--- Microservices/event-service/src/main/java/app/sportahub/eventservice/model/social/Comment.java
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/model/social/Comment.java
@@ -4,11 +4,13 @@ import app.sportahub.eventservice.model.BaseEntity;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @EqualsAndHashCode(callSuper = false)
 @SuperBuilder(toBuilder = true, setterPrefix = "with")
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(collection = "comments")
 @ToString
 @Data
 public class Comment extends BaseEntity {
