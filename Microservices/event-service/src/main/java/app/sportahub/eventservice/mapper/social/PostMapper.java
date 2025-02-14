c3ccbfa2371a5ce5c3b7a371cf83b64975a90054
Walid <walidoo_2002@hotmail.com>
2025-02-13 23:32:30 -0500

    GH-323: [feat] Add mapper for posts

---
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/mapper/social/PostMapper.java
@@ -0,0 +1,16 @@
+package app.sportahub.eventservice.mapper.social;
+
+import app.sportahub.eventservice.dto.request.social.PostRequest;
+import app.sportahub.eventservice.model.social.Post;
+import org.mapstruct.Builder;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", builder = @Builder(disableBuilder = true))
+public interface PostMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "creationDate", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    Post postRequestToPost(PostRequest postRequest);
+}
