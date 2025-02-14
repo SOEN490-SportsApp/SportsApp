0f50d60bce131279f9f2c78fefbdd7414da713db
Walid <walidoo_2002@hotmail.com>
2025-02-14 17:31:40 -0500

    GH-323: [feat] Add endpoint to retrieve posts associated to an event in order of earliest to latest

--- Microservices/event-service/src/main/java/app/sportahub/eventservice/controller/social/PostController.java
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/controller/social/PostController.java
@@ -6,6 +6,8 @@ import app.sportahub.eventservice.service.social.PostService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,5 +25,17 @@ public class PostController {
             description = "Creates a new post resource to the database based on the provided post details.")
     public Post createPost(@PathVariable String eventId, @RequestBody PostRequest postRequest) {
         return postService.createPost(eventId, postRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Retrieve all posts of an event",
+            description = "Fetches all posts associated with the specified event in order of latest creation.")
+    public Page<Post> getAllPostsOrderedByCreationDateInDesc(
+            @PathVariable String eventId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        Pageable pageable = Pageable.ofSize(size).withPage(page);
+        return postService.getAllPostsOrderedByCreationDateInDesc(eventId, pageable);
     }
 }
