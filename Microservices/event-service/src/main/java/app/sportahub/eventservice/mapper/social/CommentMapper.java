3e7fed78f7ac18019dab11bbec9de46378deb56c
Walid <walidoo_2002@hotmail.com>
2025-02-14 16:24:13 -0500

    GH-323: [feat] Add a comment mapper

---
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/mapper/social/CommentMapper.java
@@ -0,0 +1,16 @@
+package app.sportahub.eventservice.mapper.social;
+
+import app.sportahub.eventservice.dto.request.social.CommentRequest;
+import app.sportahub.eventservice.model.social.Comment;
+import org.mapstruct.Builder;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", builder = @Builder(disableBuilder = true))
+public interface CommentMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "creationDate", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    Comment commentRequestToComment(CommentRequest commentRequest);
+}
