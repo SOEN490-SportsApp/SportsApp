238d0a2def583813eeb448b2977114499b1cd356
Walid <walidoo_2002@hotmail.com>
2025-02-13 23:25:06 -0500

    GH-323: [feat] Add post, comment models to event model

---
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/dto/request/social/PostRequest.java
@@ -0,0 +1,22 @@
+package app.sportahub.eventservice.dto.request.social;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record PostRequest(
+        @NotBlank(message = "Post content must be provided")
+        String content,
+
+        @NotBlank(message = "Valid id of the user who created the post must be provided")
+        String createdBy,
+
+        @Nullable
+        List<String> attachments,
+
+        @Nullable
+        List<CommentRequest> comments) {
+}
