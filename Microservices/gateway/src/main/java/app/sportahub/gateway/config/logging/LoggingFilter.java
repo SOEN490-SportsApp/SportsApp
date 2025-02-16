9226bb5e3e5bae00f74a2d1654b7315abe955706
patrickmac3 <patrickmaceachen9@gmail.com>
2025-02-16 13:37:58 -0500

    GH-286: [feat] implemented ELK logging in the gateway service

---
+++ Microservices/gateway/src/main/java/app/sportahub/gateway/config/logging/LoggingFilter.java
@@ -0,0 +1,31 @@
+package app.sportahub.gateway.config.logging;
+
+import java.io.IOException;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Component
+public class LoggingFilter implements Filter {
+
+    private static final String REQUEST_ID = "endpoint";
+    
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        if (request instanceof HttpServletRequest httpServletRequest) {
+            String endpoint = httpServletRequest.getMethod() + " " + httpServletRequest.getRequestURI();
+            MDC.put(REQUEST_ID, endpoint); 
+        }
+        try {
+            chain.doFilter(request, response);
+        } finally {
+            MDC.clear();
+        }
+    }
+}
