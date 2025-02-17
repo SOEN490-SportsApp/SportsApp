60242aaedd28f031714fa3884e60f1cc30e909c3
wade3hamati <wade3_hamati@outlook.com>
2025-02-16 22:32:44 -0500

    GH-322: [feat] Resolve some comments on PR.

--- Microservices/event-service/src/main/java/app/sportahub/eventservice/dto/response/ReactionResponse.java
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/dto/response/ReactionResponse.java
@@ -4,17 +4,12 @@ import app.sportahub.eventservice.model.event.reactor.ReactionType;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.constraints.NotBlank;
 
-import java.time.LocalDateTime;
-
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ReactionResponse(
         @NotBlank(message = "Valid user id must be provided")
         String userId,
 
         @NotBlank(message = "Reaction type must be provided")
-        ReactionType reactionType,
-
-        @NotBlank(message = "Date user reacted must be provided")
-        LocalDateTime reactionDate
+        ReactionType reactionType
 ) {
 }
