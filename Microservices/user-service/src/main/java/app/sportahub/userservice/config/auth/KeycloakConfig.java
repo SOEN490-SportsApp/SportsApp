7ad53d4f87879fccd994916ffc8e29784aa194ee
patrickmac3 <patrickmaceachen9@gmail.com>
2025-02-17 22:54:22 -0500

    GH-287: [feat] implemented keycloak service with the keycloak admin client

--- Microservices/user-service/src/main/java/app/sportahub/userservice/config/auth/KeycloakConfig.java
+++ Microservices/user-service/src/main/java/app/sportahub/userservice/config/auth/KeycloakConfig.java
@@ -1,7 +1,12 @@
 package app.sportahub.userservice.config.auth;
 
 import lombok.Data;
+
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Data
@@ -9,8 +14,26 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "keycloak")
 public class KeycloakConfig {
 
+    @Value("${keycloak.auth-server-url}")
     private String authServerUrl;
+
+    @Value("${keycloak.realm}")
     private String realm;
+
+    @Value("${keycloak.resource}")
     private String adminClientId;
+
+    @Value("${keycloak.credentials.secret}")
     private String adminClientSecret;
+
+    @Bean
+    public Keycloak keycloak() {
+        return KeycloakBuilder.builder()
+                .clientSecret(adminClientSecret)
+                .clientId(adminClientId)
+                .grantType("client_credentials")
+                .realm(realm)
+                .serverUrl(authServerUrl)
+                .build();
+    }
 }
