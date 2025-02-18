878f5ee9c1e79f60a2fb9558ef2ff57d60a7e027
Walid <walidoo_2002@hotmail.com>
2025-02-18 15:19:10 -0500

    GH-213: [refactor] Allow all subpaths of actuator in the securityFilterChain

--- Microservices/storage-service/src/main/java/app/sportahub/storageservice/config/auth/SecurityConfig.java
+++ Microservices/storage-service/src/main/java/app/sportahub/storageservice/config/auth/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
                                 "/api-docs/**",
                                 "/swagger-ui/**",
                                 "/webjars/**",
-                                "/actuator/health/**")
+                                "/actuator/**")
                         .permitAll()
                         .anyRequest().authenticated())
                 .csrf(AbstractHttpConfigurer::disable)
