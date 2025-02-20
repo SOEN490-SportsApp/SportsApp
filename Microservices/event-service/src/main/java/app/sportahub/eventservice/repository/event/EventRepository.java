0d88eaa244726acfb90bfff494cd941aec0401c1
wade3hamati <wade3_hamati@outlook.com>
2025-02-20 08:28:53 -0500

    GH-337: [feat] Create and implement searching repository.

--- Microservices/event-service/src/main/java/app/sportahub/eventservice/repository/event/EventRepository.java
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/repository/event/EventRepository.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface EventRepository extends MongoRepository<Event, String> {
+public interface EventRepository extends MongoRepository<Event, String>, SearchingEventRepository {
 
     Optional<Event> findEventById(String id);
 
