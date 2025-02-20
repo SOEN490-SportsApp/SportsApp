7650129f75c5d411ea2568b01b422576ebc2043a
wade3hamati <wade3_hamati@outlook.com>
2025-02-19 22:00:55 -0500

    GH-0: [refactor] Fix failing test and update leaveEvent logic.

--- Microservices/event-service/src/test/java/app/sportahub/eventservice/service/event/EventServiceTest.java
+++ Microservices/event-service/src/test/java/app/sportahub/eventservice/service/event/EventServiceTest.java
@@ -29,6 +29,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -410,6 +411,7 @@ class EventServiceTest {
         event.setId(eventId);
         event.setDate(eventDate);
         event.setCutOffTime(cutOffTime.toString());
+        event.setStartTime(LocalTime.now().plusHours(1));
         event.setParticipants(new ArrayList<>(List.of(participant)));
 
         when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
@@ -460,7 +462,8 @@ class EventServiceTest {
         String userId = "user456";
         Event event = new Event();
         event.setId(eventId);
-        event.setDate(LocalDate.now().minusDays(1)); // Event already started
+        event.setStartTime(LocalTime.now().minusHours(1)); // Event already started
+        event.setCutOffTime(LocalDateTime.now().minusDays(2).toString());
         event.setParticipants(new ArrayList<>(List.of(new Participant(userId, ParticipantAttendStatus.JOINED, LocalDate.now()))));
 
         when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
@@ -497,6 +500,7 @@ class EventServiceTest {
         Event event = new Event();
         event.setId(eventId);
         event.setDate(LocalDate.now().plusDays(1));
+        event.setStartTime(LocalTime.now().plusHours(1));
         event.setCutOffTime(pastCutOffTime.toString());
         event.setParticipants(new ArrayList<>(List.of(participant)));
 
