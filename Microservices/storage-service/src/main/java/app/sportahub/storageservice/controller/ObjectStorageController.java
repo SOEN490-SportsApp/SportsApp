08d3016171cccbea0b1f768cc6d47be052c6e54c
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-02-21 21:50:44 -0500

    GH-0: [refactor] Move downloadUrl structuring to service

--- Microservices/storage-service/src/main/java/app/sportahub/storageservice/controller/ObjectStorageController.java
+++ Microservices/storage-service/src/main/java/app/sportahub/storageservice/controller/ObjectStorageController.java
@@ -1,16 +1,12 @@
 package app.sportahub.storageservice.controller;
 
 import app.sportahub.storageservice.dto.response.ObjectResponse;
-import app.sportahub.storageservice.model.MinioObjectMetadata;
 import app.sportahub.storageservice.service.ObjectStorageService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/objects")
@@ -20,15 +16,9 @@ public class ObjectStorageController {
 
     @PostMapping("/upload")
     @ResponseStatus(HttpStatus.CREATED)
-    @Operation(summary = "Creates a new event",
-            description = "Creates a new event resource to the database based on the provided event details.")
+    @Operation(summary = "Uploads a file",
+            description = "Uploads a file to MinIO and returns metadata including the public download URL.")
     public ObjectResponse uploadFile(@RequestParam("file") MultipartFile file) {
-        MinioObjectMetadata minioObjectMetadataInfo = objectStorageService.storeFile(file);
-        String downloadUrl = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path("/objects/download/")
-                .path(minioObjectMetadataInfo.getFileName())
-                .toUriString();
-
-        return new ObjectResponse(file.getName(), file.getContentType(), file.getSize(), LocalDateTime.now(), downloadUrl);
+        return objectStorageService.storeFile(file);
     }
 }
