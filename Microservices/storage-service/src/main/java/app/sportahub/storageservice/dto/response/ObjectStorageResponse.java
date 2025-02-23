e6a266ad6d1ddadb53654caab2bb230e4a1cc7d0
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-02-22 23:10:39 -0500

    GH-0: [chore] Rename response arg

--- Microservices/storage-service/src/main/java/app/sportahub/storageservice/dto/response/ObjectStorageResponse.java
+++ Microservices/storage-service/src/main/java/app/sportahub/storageservice/dto/response/ObjectStorageResponse.java
@@ -3,5 +3,5 @@ package app.sportahub.storageservice.dto.response;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record ObjectStorageResponse(String fileName, String ownerId, String contentType, long size, String downloadUrl) {
+public record ObjectStorageResponse(String fileName, String ownerId, String contentType, long size, String downloadPath) {
 }
