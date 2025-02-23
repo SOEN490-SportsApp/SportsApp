9a03e8f29a96c39ad90672b9598cbf0f9b25929c
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-02-22 23:05:43 -0500

    GH-0: [chore] Remove unused files

--- Microservices/storage-service/src/main/java/app/sportahub/storageservice/service/file/MimeTypeService.java
+++
@@ -1,7 +0,0 @@
-package app.sportahub.storageservice.service.file;
-
-import java.io.InputStream;
-
-public interface MimeTypeService {
-    String getMimeTypeFromInputStream(InputStream inputStream);
-}
