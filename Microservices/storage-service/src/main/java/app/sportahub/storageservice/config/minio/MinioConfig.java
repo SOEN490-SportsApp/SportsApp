e33573343c6be0ccf576078b7a97c6e9c5e4150f
Walid <walidoo_2002@hotmail.com>
2025-02-23 00:48:04 -0500

    GH-0: [fix] Add @Generated annotation to MinioConfig and SecurityConfig classes

--- Microservices/storage-service/src/main/java/app/sportahub/storageservice/config/minio/MinioConfig.java
+++ Microservices/storage-service/src/main/java/app/sportahub/storageservice/config/minio/MinioConfig.java
@@ -3,6 +3,7 @@ package app.sportahub.storageservice.config.minio;
 import io.minio.BucketExistsArgs;
 import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
+import lombok.Generated;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Slf4j
+@Generated
 @Configuration
 public class MinioConfig {
     @Value("${minio.url}")
