79a8299c5a3bd19b769968723710ef17d63e430b
JRB958 <141108231+JRB958@users.noreply.github.com>
2025-02-26 23:21:13 -0500

    GH-345: [feat] Update the Redux store with a location slice

--- ClientApp/state/store.ts
+++ ClientApp/state/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '@/state/user/userSlice';
 import notificationsReducer from "@/state/notifications/notificationSlice";
+import locationReducer from "@/state/location/locationSlice";
 
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     notifications: notificationsReducer,
+    location: locationReducer,
   },
 });
 
