c26576cae2f6482c1fea4e9015c793c07b6a60d7
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-03-11 17:02:16 -0400

    GH-382: [refactor] Rename package for notification service to improve consistency

--- Microservices/notification-service/src/main/java/app/sportahub/notificationservice/NotificationServiceApplication.java
+++ Microservices/notification-service/src/main/java/app/sportahub/notificationservice/NotificationServiceApplication.java
@@ -1,4 +1,4 @@
-package app.sportahub.notification_service;
+package app.sportahub.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
