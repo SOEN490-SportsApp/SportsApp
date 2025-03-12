cf0059aa6e38cd8430af6a86a9d9ce1994119df5
wade3hamati <wade3_hamati@outlook.com>
2025-03-12 09:34:40 -0400

    GH-298: [feat] Add forgotPassword package.

---
+++ Microservices/kafka-events/src/main/java/app/sportahub/kafkevents/forgotPassword/ForgotPasswordRequestedEvent.java
@@ -0,0 +1,21 @@
+package app.sportahub.kafkevents.forgotPassword;
+
+import app.sportahub.kafkevents.BaseEvent;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ForgotPasswordRequestedEvent {
+    private BaseEvent baseEvent;
+    private String email;
+
+    @JsonCreator
+    public ForgotPasswordRequestedEvent(
+            @JsonProperty("baseEvent") BaseEvent baseEvent,
+            @JsonProperty("email") String email)
+    {
+        this.baseEvent = baseEvent;
+        this.email = email;
+    }
+    public String getEmail() {return this.email;}
+    public BaseEvent getBaseEvent() {return this.baseEvent;}
+}
