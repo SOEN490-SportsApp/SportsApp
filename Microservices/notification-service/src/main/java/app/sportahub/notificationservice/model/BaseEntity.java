81bfe0cba6a3c3f86f352d657d44b328da2042f1
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-03-11 21:30:23 -0400

    GH-382: [feat] Add BaseEntity and Notification model classes for notification service

---
+++ Microservices/notification-service/src/main/java/app/sportahub/notificationservice/model/BaseEntity.java
@@ -0,0 +1,25 @@
+package app.sportahub.notificationservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.sql.Timestamp;
+
+@SuperBuilder(toBuilder = true, setterPrefix = "with")
+@ToString(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class BaseEntity {
+
+    @MongoId(FieldType.OBJECT_ID)
+    private String id;
+
+    private Timestamp creationDate;
+    private Timestamp updatedAt;
+}
