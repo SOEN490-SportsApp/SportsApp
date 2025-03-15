340786e6ffb75599c665573db43ec6369fa8b2a4
wade3hamati <wade3_hamati@outlook.com>
2025-03-15 09:11:31 -0400

    GH-298: [feat] Remove unused statement and constructor.

--- Microservices/email-service/src/main/java/app/sportahub/emailservice/service/kafka/consumer/OrchestrationServiceConsumerImpl.java
+++ Microservices/email-service/src/main/java/app/sportahub/emailservice/service/kafka/consumer/OrchestrationServiceConsumerImpl.java
@@ -1,7 +1,6 @@
 package app.sportahub.emailservice.service.kafka.consumer;
 
 import app.sportahub.kafkevents.forgotPassword.ForgotPasswordSendEmailEvent;
-import app.sportahub.emailservice.service.EmailService;
 import app.sportahub.kafkevents.forgotPassword.ForgotPasswordEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,8 +11,6 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class OrchestrationServiceConsumerImpl implements OrchestrationServiceConsumer {
-
-    private final EmailService emailService;
 
     @Override
     @KafkaListener(topics = ForgotPasswordEvent.SEND_EMAIL_TOPIC, groupId ="OrchServiceKafkaConsumer")
--- Microservices/kafka-events/src/main/java/app/sportahub/kafkevents/forgotPassword/ForgotPasswordEvent.java
+++ Microservices/kafka-events/src/main/java/app/sportahub/kafkevents/forgotPassword/ForgotPasswordEvent.java
@@ -3,6 +3,4 @@ package app.sportahub.kafkevents.forgotPassword;
 public final class ForgotPasswordEvent {
     public static final String SEND_REQUEST_TOPIC = "forgot-password.request";
     public static final String SEND_EMAIL_TOPIC = "forgot-password.send-email";
-
-    ForgotPasswordEvent() {}
 }
