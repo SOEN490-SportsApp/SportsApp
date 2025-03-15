150bc567dd4458979bb62851da82fd42ec5feb22
Walid <walidoo_2002@hotmail.com>
2025-03-15 18:00:49 -0400

    GH-382: [feat] Add Notification service, repository, mapper, and exception handling for managing user notifications

--- Microservices/notification-service/src/main/java/app/sportahub/notificationservice/dto/request/notification/NotificationRequest.java
+++ Microservices/notification-service/src/main/java/app/sportahub/notificationservice/dto/request/notification/NotificationRequest.java
@@ -1,7 +1,24 @@
 package app.sportahub.notificationservice.dto.request.notification;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Null;
+
 import java.util.Map;
 
-public record NotificationRequest(String userId, String title, String body, String clickAction, String icon,
+public record NotificationRequest(@NotBlank
+                                  String userId,
+
+                                  @NotBlank
+                                  String title,
+
+                                  @NotBlank
+                                  String body,
+
+                                  @NotBlank
+                                  String clickAction,
+
+                                  @Null
+                                  String icon,
+
                                   Map<String, String> data) {
 }
