8db1bb55f1167cba24ce881070a15d11ad68667e
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-03-20 00:06:19 -0400

    GH-382: [feat] Add KafkaProducerConfig for configuring Kafka producer settings

--- Microservices/user-service/src/main/java/app/sportahub/userservice/config/kafka/KafkaProducerConfig.java
+++ Microservices/user-service/src/main/java/app/sportahub/userservice/config/kafka/KafkaProducerConfig.java
@@ -14,7 +14,6 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 import java.util.HashMap;
 import java.util.Map;
 
-@EnableKafka
 @Configuration
 public class KafkaProducerConfig {
 
