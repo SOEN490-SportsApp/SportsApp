a8008215fd7f3953601580cf33ca1e1138992f63
wade3hamati <wade3_hamati@outlook.com>
2025-03-21 10:07:16 -0400

    GH-384: [feat] Initial attempt to get eventIds from the event-service.

--- Microservices/orchestration-service/src/main/java/app/sportahub/orchestrationservice/service/consumer/UserServiceConsumer.java
+++ Microservices/orchestration-service/src/main/java/app/sportahub/orchestrationservice/service/consumer/UserServiceConsumer.java
@@ -1,4 +1,9 @@
 package app.sportahub.orchestrationservice.service.consumer;
 
+import app.sportahub.kafkevents.JoinedSportEventEvent.JoinedEventsByUserRequestEvent;
+import app.sportahub.kafkevents.forgotPassword.ForgotPasswordRequestedEvent;
+
 public interface UserServiceConsumer {
+    public void listenForForgotPasswordRequestedEvent(ForgotPasswordRequestedEvent forgotPasswordRequestedEvent);
+    public void listenForJoinedEventsByUserRequestEvent(JoinedEventsByUserRequestEvent requestEvent);
 }
--- Microservices/orchestration-service/src/main/java/app/sportahub/orchestrationservice/service/consumer/UserServiceConsumerImpl.java
+++ Microservices/orchestration-service/src/main/java/app/sportahub/orchestrationservice/service/consumer/UserServiceConsumerImpl.java
@@ -1,6 +1,7 @@
 package app.sportahub.orchestrationservice.service.consumer;
 
 import app.sportahub.kafkevents.BaseEvent;
+import app.sportahub.kafkevents.JoinedSportEventEvent.*;
 import app.sportahub.kafkevents.forgotPassword.ForgotPasswordRequestedEvent;
 import app.sportahub.kafkevents.forgotPassword.ForgotPasswordSendEmailEvent;
 import app.sportahub.kafkevents.forgotPassword.ForgotPasswordEvent;
@@ -8,18 +9,36 @@ import app.sportahub.orchestrationservice.service.producer.EmailServiceProducer;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.errors.TimeoutException;
+import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
+import org.springframework.kafka.requestreply.RequestReplyFuture;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import static app.sportahub.kafkevents.JoinedSportEventEvent.JoinedEventsByUserEvent.FETCHED_TOPIC;
+import static app.sportahub.kafkevents.JoinedSportEventEvent.JoinedEventsByUserEvent.REQUEST_TOPIC;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class UserServiceConsumerImpl implements UserServiceConsumer {
+public class UserServiceConsumerImpl{
 
     private final EmailServiceProducer emailServiceProducer;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final ReplyingKafkaTemplate<String, Object, Object> replyingKafkaTemplate;
 
     @SneakyThrows
     @KafkaListener(topics = ForgotPasswordEvent.SEND_REQUEST_TOPIC, groupId ="UserServiceKafkaConsumer")
@@ -38,5 +57,83 @@ public class UserServiceConsumerImpl implements UserServiceConsumer {
                 forgotPasswordRequestedEvent.getEmail()
         );
         emailServiceProducer.sendForgotPasswordSendEmailEvent(forgotPasswordSendEmailEvent);
+    }
+
+    @SneakyThrows
+    @KafkaListener(topics = {REQUEST_TOPIC, FETCHED_TOPIC} , groupId = "UserServiceKafkaConsumer")
+    public void listenForJoinedEventsByUserRequestEvent(
+            @Payload JoinedEventsByUserRequestEvent requestEvent,
+            @Header(KafkaHeaders.CORRELATION_ID) byte[] correlationId){
+        String userId = requestEvent.getEmail();
+
+        BaseEvent fetchBaseEvent = new BaseEvent(
+                UUID.randomUUID().toString(),
+                "response",
+                "orchestration-service",
+                Instant.now(),
+                requestEvent.getBaseEvent().getCorrelationId()
+        );
+
+        JoinedEventsByUserFetchEvent fetchEvent = new JoinedEventsByUserFetchEvent(fetchBaseEvent, userId);
+
+        // Send the fetch event and wait for the response
+//        ProducerRecord<String, Object> record = new ProducerRecord<>(
+//                JoinedEventsByUserEvent.FETCH_TOPIC,
+//                fetchEvent
+//        );
+//        record.headers().add(new RecordHeader(KafkaHeaders.REPLY_TOPIC, JoinedEventsByUserEvent.FETCHED_TOPIC.getBytes()));
+//        record.headers().add(new RecordHeader(KafkaHeaders.CORRELATION_ID, fetchEvent.getBaseEvent().getCorrelationId().getBytes()));
+
+        JoinedEventsByUserFetchedEvent fetchedEvent = null;
+        try{
+            replyingKafkaTemplate.start();
+            ProducerRecord<String, Object> record = new ProducerRecord<>(
+                    JoinedEventsByUserEvent.FETCH_TOPIC,
+                    fetchEvent
+            );
+            record.headers().add(new RecordHeader(KafkaHeaders.REPLY_TOPIC, JoinedEventsByUserEvent.FETCHED_TOPIC.getBytes()));
+            record.headers().add(new RecordHeader(KafkaHeaders.CORRELATION_ID, correlationId));
+
+            RequestReplyFuture<String, Object, Object> future =
+                    replyingKafkaTemplate.sendAndReceive(record);
+            log.info("UserServiceConsumerImpl::listenForJoinedEventsByUserRequestEvent: fetch topic");
+
+            SendResult<String, Object> sendResult = future.getSendFuture().get();
+            sendResult.getProducerRecord().headers().forEach(header -> System.out.println(header.key() + ":" + header.value().toString()));
+
+            ConsumerRecord<String, Object> response = future.get(90, TimeUnit.SECONDS);
+            log.info("UserServiceConsumerImpl::listenForJoinedEventsByUserRequestEvent: fetched topic");
+
+            fetchedEvent = (JoinedEventsByUserFetchedEvent) response.value();
+
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+        } catch (ExecutionException e) {
+            log.error(e.getMessage());
+        } catch (TimeoutException e) {
+            log.error(e.getMessage());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        } finally {
+            replyingKafkaTemplate.stop();
+        }
+
+        if(fetchedEvent != null){
+
+            BaseEvent fetchedBaseEvent = new BaseEvent(
+                    UUID.randomUUID().toString(),
+                    "response",
+                    "orchestration-service",
+                    Instant.now(),
+                    fetchedEvent.getBaseEvent().getCorrelationId()
+            );
+            JoinedEventsByUserResponseEvent responseEvent = new JoinedEventsByUserResponseEvent(fetchedBaseEvent, fetchedEvent.getEventIds());
+
+            ProducerRecord<String, Object> responseRecord = new ProducerRecord<>(JoinedEventsByUserEvent.RESPONSE_TOPIC, responseEvent);
+            responseRecord.headers().add(new RecordHeader(KafkaHeaders.CORRELATION_ID, correlationId));
+            kafkaTemplate.send(responseRecord);
+
+            log.info("UserServiceConsumerImpl::listenForJoinedEventsByUserRequestEvent: response topic sent");
+        }
     }
 }
