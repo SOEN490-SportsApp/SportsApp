3214fa290984ec345c43f0b4d94e4b7d4ea1194e
wade3hamati <wade3_hamati@outlook.com>
2025-03-21 10:04:30 -0400

    GH-384: [feat] Create events for kafka workflow.

---
+++ libs/kafka-events/src/main/java/app/sportahub/kafkevents/JoinedSportEventEvent/JoinedEventsByUserEvent.java
@@ -0,0 +1,8 @@
+package app.sportahub.kafkevents.JoinedSportEventEvent;
+
+public final class JoinedEventsByUserEvent {
+    public static final String REQUEST_TOPIC = "joined-events-by-user.request";
+    public static final String RESPONSE_TOPIC = "joined-events-by-user.response";
+    public static final String FETCH_TOPIC = "joined-events-by-user.fetch";
+    public static final String FETCHED_TOPIC = "joined-events-by-user.fetched";
+}
