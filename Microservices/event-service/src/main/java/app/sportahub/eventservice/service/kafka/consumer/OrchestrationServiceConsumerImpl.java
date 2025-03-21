515af415be78509d00baed769543362d7d30c4d7
wade3hamati <wade3_hamati@outlook.com>
2025-03-21 13:21:11 -0400

    GH-384: [feat] Cleanup Listener method.

--- Microservices/event-service/src/main/java/app/sportahub/eventservice/service/kafka/consumer/OrchestrationServiceConsumerImpl.java
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/service/kafka/consumer/OrchestrationServiceConsumerImpl.java
@@ -8,22 +8,16 @@ import app.sportahub.kafkevents.JoinedSportEventEvent.JoinedEventsByUserFetchEve
 import app.sportahub.kafkevents.JoinedSportEventEvent.JoinedEventsByUserFetchedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.nio.charset.StandardCharsets;
 import java.time.Instant;
-import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -33,7 +27,6 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class OrchestrationServiceConsumerImpl implements OrchestrationServiceConsumer {
 
-    //private final ReplyingKafkaTemplate<String, Object, Object> replyingKafkaTemplate;
     private final KafkaTemplate<String, Object> kafkaTemplate;
     private final EventRepository eventRepository;
 
@@ -41,64 +34,28 @@ public class OrchestrationServiceConsumerImpl implements OrchestrationServiceCon
     public void handleEventsByUserRequest(
             @Payload JoinedEventsByUserFetchEvent fetchEvent,
             @Header(KafkaHeaders.CORRELATION_ID) byte[] correlationId) {
-        log.info("OrchestrationServiceConsumerImpl::handleEventsByUserRequest: fetch topic received");
+        log.info("OrchestrationServiceConsumerImpl::handleEventsByUserRequest: received fetch request for event ids for user with id: {}", fetchEvent.getUserId());
 
-        String correlationIdStr = new String(correlationId);
-        log.info("Received request with correlationId: {}", correlationIdStr);
-
-        String userId = fetchEvent.getEmail();
+        String userId = fetchEvent.getUserId();
 
         List<String> eventIds = eventRepository.findAllByParticipantUserId(userId)
                 .stream()
                 .map(Event::getId)
                 .collect(Collectors.toList());
 
-        log.info("eventIds: {}", eventIds);
-
         BaseEvent responseBaseEvent = new BaseEvent(
                 UUID.randomUUID().toString(),
                 "response",
                 "event-service",
                 Instant.now(),
-                correlationIdStr
+                fetchEvent.getBaseEvent().getCorrelationId()
         );
 
         JoinedEventsByUserFetchedEvent fetchedEvent = new JoinedEventsByUserFetchedEvent(responseBaseEvent, eventIds);
-
-        // Manually produce the response
         ProducerRecord<String, Object> responseRecord = new ProducerRecord<>(JoinedEventsByUserEvent.FETCHED_TOPIC, fetchedEvent);
         responseRecord.headers().add(new RecordHeader(KafkaHeaders.CORRELATION_ID, correlationId));
-        log.info("corr: {}",correlationId);
         kafkaTemplate.send(responseRecord);
-
-//        replyingKafkaTemplate.send(JoinedEventsByUserEvent.FETCHED_TOPIC, fetchedEvent);
-//        kafkaTemplate.send(JoinedEventsByUserEvent.FETCHED_TOPIC, fetchedEvent);
-
-//        kafkaTemplate.send("joined-events-by-user.fetched", fetchedEvent)
-//                .thenAccept(result -> {
-//                    if (result != null) {
-//                        // Add the correlation ID to the reply message
-//                        result.getProducerRecord().headers().add(
-//                                KafkaHeaders.CORRELATION_ID,
-//                                correlationId // Use the correlation ID from the request
-//                        );
-//                        log.info("EventService: Sent fetched events for user {} with correlationId {}", fetchEvent.getEmail(), new String(correlationId, StandardCharsets.UTF_8));
-//                    }
-//                })
-//                .exceptionally(ex -> {
-//                    log.error("EventService: Failed to send fetched events for user {}", fetchEvent.getEmail(), ex);
-//                    return null;
-//                });
-
-//        replyingKafkaTemplate.executeInTransaction(template -> {
-//            template.send(JoinedEventsByUserEvent.FETCHED_TOPIC, fetchedEvent);
-//            return Collections.<String> emptyList();
-//        });
-//        log.info("OrchestrationServiceConsumerImpl::handleEventsByUserRequest: fetched topic sent");
-
-        // Send response to orchestration-service
-//        replyingKafkaTemplate.send(JoinedEventsByUserEvent.FETCHED_TOPIC, fetchedEvent);
-//        log.info("OrchestrationServiceConsumerImpl::handleEventsByUserRequest: fetched topic sent");
+        log.info("OrchestrationServiceConsumerImpl::handleEventsByUserRequest: sent event ids for user with id: {}", fetchEvent.getUserId());
     }
 
 }
