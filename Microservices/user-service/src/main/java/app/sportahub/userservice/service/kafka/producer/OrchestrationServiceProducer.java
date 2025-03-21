a341b681b73f738694865c1bdb3210f22f5cce04
wade3hamati <wade3_hamati@outlook.com>
2025-03-21 10:11:04 -0400

    GH-384: [feat] Setup user-service to get eventIds for a user to use in the friend recommendation system.

--- Microservices/user-service/src/main/java/app/sportahub/userservice/service/kafka/producer/OrchestrationServiceProducer.java
+++ Microservices/user-service/src/main/java/app/sportahub/userservice/service/kafka/producer/OrchestrationServiceProducer.java
@@ -1,7 +1,10 @@
 package app.sportahub.userservice.service.kafka.producer;
 
+import org.apache.kafka.common.errors.TimeoutException;
+import java.util.List;
 
 public interface OrchestrationServiceProducer {
 
     void sendPasswordResetEmailUsingKafka(String email);
+    List<String> getEventsJoinedByUser(String userId);
 }
