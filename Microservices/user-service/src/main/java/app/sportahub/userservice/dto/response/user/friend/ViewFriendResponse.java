53a90ba095a083b17f753a7dc47fadc40ed2f7fa
wolfie7679 <90850098+wolfie7679@users.noreply.github.com>
2025-03-21 17:39:24 -0400

    GH-339: [feat] - removed ViewFriendResponse profile picture as this is for another PR.

--- Microservices/user-service/src/main/java/app/sportahub/userservice/dto/response/user/friend/ViewFriendResponse.java
+++ Microservices/user-service/src/main/java/app/sportahub/userservice/dto/response/user/friend/ViewFriendResponse.java
@@ -1,4 +1,4 @@
 package app.sportahub.userservice.dto.response.user.friend;
 
-public record ViewFriendResponse(String friendUsername, String friendUserId, String FriendId,String profilePictureURL) {
+public record ViewFriendResponse(String friendUsername, String friendUserId, String FriendId) {
 }
