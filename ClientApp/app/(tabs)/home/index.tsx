a509129f72b253c57ae86ce12c182f8c53cd3fee
JRB958 <141108231+JRB958@users.noreply.github.com>
2025-03-22 17:38:04 -0400

    GH-345: [fix] Fixed the order of events in the homepage

--- ClientApp/app/(tabs)/home/index.tsx
+++ ClientApp/app/(tabs)/home/index.tsx
@@ -9,35 +9,8 @@ import { Event } from "@/types/event";
 import { useSelector } from "react-redux";
 
 const HomePage = () => {
-    const scrollY = useRef(new Animated.Value(0)).current;
-    const [refreshing, setRefreshing] = React.useState(false);
-    const [events, setEvents] = useState<Event[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [isCalendarVisible, setIsCalenarVisible] = useState(false)
-    const user = useSelector((state: { user: any }) => state.user);
-  const fetchEvents = async () => {
-      try {
-        setLoading(true);
-        const eventsData = await getAllEvents();
-  
-        const validEvents = eventsData.filter((event: Event) => {
-          const cutoffTime = new Date(event.cutOffTime);
-          return !isNaN(cutoffTime.getTime());
-        });
-  
-        setEvents(validEvents);
-      } catch (error) {
-        Alert.alert("Error", "Failed to fetch events. Please try again later.");
-      } finally {
-        setLoading(false);
-      }
-    };
-  
-    useEffect(() => {
-      fetchEvents();
-    }, []);
-
-    
+  const [isCalendarVisible, setIsCalenarVisible] = useState(false)
+  const user = useSelector((state: { user: any }) => state.user);
 
   return (
     <SafeAreaView style={styles.container}>
