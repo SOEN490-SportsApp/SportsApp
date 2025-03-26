8674859c31e647fe7fc624e65fe6e7c127b07e9a
patrickmac3 <patrickmaceachen9@gmail.com>
2025-03-26 00:08:05 -0400

    GH-424: [feat] Changes to kafka event library to add events related to getting a user profile from the event service

---
+++ libs/kafka-events/src/main/java/app/sportahub/kafkevents/user/UserEvent.java
@@ -0,0 +1,8 @@
+package app.sportahub.kafkevents.user;
+
+public class UserEvent {
+    public static final String REQUEST_TOPIC = "user.request";
+    public static final String RESPONSE_TOPIC = "user.response";
+    public static final String FETCH_TOPIC = "user.fetch";
+    public static final String FETCHED_TOPIC = "user.fetched";
+}
