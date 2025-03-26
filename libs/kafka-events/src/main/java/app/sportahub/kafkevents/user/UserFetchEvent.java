42532ca11e545e2c7f5e50a12f80b8c7805cce08
patrickmac3 <patrickmaceachen9@gmail.com>
2025-03-26 00:08:05 -0400

    GH-424: [feat] Changes to kafka event library to add events related to getting a user profile from the event service

---
+++ libs/kafka-events/src/main/java/app/sportahub/kafkevents/user/UserFetchEvent.java
@@ -0,0 +1,24 @@
+package app.sportahub.kafkevents.user;
+
+import app.sportahub.kafkevents.BaseEvent;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.lang.String;
+
+
+public class UserFetchEvent {
+    private final BaseEvent baseEvent;
+    private final String userId;
+
+    @JsonCreator
+    public UserFetchEvent(
+            @JsonProperty("baseEvent") BaseEvent baseEvent,
+            @JsonProperty("userId") String userId)
+    {
+        this.baseEvent = baseEvent;
+        this.userId = userId;
+    }
+    public String getUserId() {return this.userId;}
+    public BaseEvent getBaseEvent() {return this.baseEvent;}
+}
