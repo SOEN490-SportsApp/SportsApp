3e45ab24539b6fb1e655c92735a4f25fd6965a50
wade3hamati <wade3_hamati@outlook.com>
2025-03-26 20:40:08 -0400

    GH-0: [feat] Create DTOs for post & comment requests and responses.

---
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/dto/response/social/CommentResponse.java
@@ -0,0 +1,11 @@
+package app.sportahub.eventservice.dto.response.social;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record CommentResponse(
+        String eventId,
+        String postId,
+        String content,
+        String createdBy) {
+}
