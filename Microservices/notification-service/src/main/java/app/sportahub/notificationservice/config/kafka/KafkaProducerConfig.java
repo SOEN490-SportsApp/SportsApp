80278dc5530c17033f4d058b6ff772bcfb3f2bf2
Walid <walidoo_2002@hotmail.com>
2025-03-29 05:30:34 -0400

    GH-382: [fix] Update Kafka configuration to use bootstrapServers variable and enhance error handling

--- Microservices/notification-service/src/main/java/app/sportahub/notificationservice/config/kafka/KafkaProducerConfig.java
+++ Microservices/notification-service/src/main/java/app/sportahub/notificationservice/config/kafka/KafkaProducerConfig.java
@@ -1,5 +1,6 @@
 package app.sportahub.notificationservice.config.kafka;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,12 +20,12 @@ import java.util.Map;
 public class KafkaProducerConfig {
 
     @Value("${spring.kafka.producer.bootstrap-servers}")
-    private String port;
+    private String bootstrapServers;
 
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, port);
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
