5e74439f956be37185c5a19c9261b9478c440e26
Walid <walidoo_2002@hotmail.com>
2025-03-29 05:30:34 -0400

    GH-382: [fix] Update Kafka configuration to use bootstrapServers variable and enhance error handling

--- Microservices/notification-service/src/main/java/app/sportahub/notificationservice/config/kafka/KafkaConsumerConfig.java
+++ Microservices/notification-service/src/main/java/app/sportahub/notificationservice/config/kafka/KafkaConsumerConfig.java
@@ -10,6 +10,7 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
@@ -21,17 +22,17 @@ import java.util.Map;
 public class KafkaConsumerConfig {
 
     @Value("${spring.kafka.consumer.bootstrap-servers}")
-    private String port;
+    private String bootstrapServers;
 
     @Bean
     public ConsumerFactory<String, Object> consumerFactory() {
 
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, port);
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        configProps.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
         configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
         configProps.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
+        configProps.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
 
         return new DefaultKafkaConsumerFactory<>(configProps, new StringDeserializer(), new JsonDeserializer<>(Object.class));
     }
@@ -40,6 +41,8 @@ public class KafkaConsumerConfig {
     public KafkaListenerContainerFactory<?> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
+        factory.setConcurrency(3);
+        factory.setCommonErrorHandler(new DefaultErrorHandler());
         return factory;
     }
 }
