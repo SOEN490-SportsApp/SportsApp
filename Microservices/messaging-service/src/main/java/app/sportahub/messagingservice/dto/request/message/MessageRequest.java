13dc435c24c769da54b60bf847eb87e0a89ce433
DanDuguay <duguay9@gmail.com>
2025-03-31 22:30:12 -0400

    GH-0: [hotfix] added new Member object to MessageRequest/MessageResponse

--- Microservices/messaging-service/src/main/java/app/sportahub/messagingservice/dto/request/message/MessageRequest.java
+++ Microservices/messaging-service/src/main/java/app/sportahub/messagingservice/dto/request/message/MessageRequest.java
@@ -1,8 +1,10 @@
 package app.sportahub.messagingservice.dto.request.message;
 
+import app.sportahub.messagingservice.model.Member;
+
 import java.util.Set;
 import java.util.List;
 
 public record MessageRequest(String chatroomId, String senderId, String senderName,
-                             Set<String> receiverIds, String content, List<String> attachments) {
+                             Set<Member> receivers, String content, List<String> attachments) {
 }
--- Microservices/messaging-service/src/main/java/app/sportahub/messagingservice/dto/response/message/MessageResponse.java
+++ Microservices/messaging-service/src/main/java/app/sportahub/messagingservice/dto/response/message/MessageResponse.java
@@ -1,5 +1,6 @@
 package app.sportahub.messagingservice.dto.response.message;
 
+import app.sportahub.messagingservice.model.Member;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import java.sql.Timestamp;
 import java.util.Set;
@@ -7,5 +8,5 @@ import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record MessageResponse(String messageId, String chatroomId, String senderId, String senderName,
-                              Set<String> receiverId, String content, Timestamp createdAt, List<String> attachments) {
+                              Set<Member> receivers, String content, Timestamp createdAt, List<String> attachments) {
 }
