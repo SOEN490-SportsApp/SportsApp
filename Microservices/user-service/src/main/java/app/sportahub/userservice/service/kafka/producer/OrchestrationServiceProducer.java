d9a5e6f918001b323db7742f9e7e0cf73b8fe6f5
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-04-02 19:11:10 -0400

    GH-382: [refactor] Remove friend request and badge assignment notification methods from OrchestrationServiceProducer

--- Microservices/user-service/src/main/java/app/sportahub/userservice/service/kafka/producer/OrchestrationServiceProducer.java
+++ Microservices/user-service/src/main/java/app/sportahub/userservice/service/kafka/producer/OrchestrationServiceProducer.java
@@ -1,12 +1,9 @@
 package app.sportahub.userservice.service.kafka.producer;
 
-import org.apache.kafka.common.errors.TimeoutException;
 import java.util.List;
 
 public interface OrchestrationServiceProducer {
 
     void sendPasswordResetEmailUsingKafka(String email);
     List<String> getEventsJoinedByUser(String userId);
-    void sendFriendRequestNotification(String senderId, String receiverId, String senderUsername);
-    void sendBadgeAssignmentNotification(String userId, String giverId, String badgeId);
 }
