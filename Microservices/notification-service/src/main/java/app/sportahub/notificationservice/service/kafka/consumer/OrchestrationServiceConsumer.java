5987bd441ab0c2ba2d4b30a5fa5d63d839d6c1ba
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-04-02 17:44:38 -0400

    GH-382: [feat] Update OrchestrationServiceConsumer to listen for NotificationEvent and process notifications accordingly

--- Microservices/notification-service/src/main/java/app/sportahub/notificationservice/service/kafka/consumer/OrchestrationServiceConsumer.java
+++ Microservices/notification-service/src/main/java/app/sportahub/notificationservice/service/kafka/consumer/OrchestrationServiceConsumer.java
@@ -1,9 +1,11 @@
 package app.sportahub.notificationservice.service.kafka.consumer;
 
 import app.sportahub.notificationservice.dto.request.notification.NotificationRequest;
+import app.sportahub.kafka.events.SportaKafkaEvents;
+import app.sportahub.kafka.events.notification.NotificationEvent;
 
 public interface OrchestrationServiceConsumer {
 
-    void consumeNotification(NotificationRequest request);
+    void listenForNotificationEvent(NotificationEvent event);
 
 }
