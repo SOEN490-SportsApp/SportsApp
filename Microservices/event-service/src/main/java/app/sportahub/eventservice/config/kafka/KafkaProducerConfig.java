badd230576dbc77501877110ce27f74b3aa81015
Walid <walidoo_2002@hotmail.com>
2025-04-02 19:39:40 -0400

    GH-0: [fix] Remove redundant kafkaTemplate bean method in KafkaProducerConfig

--- Microservices/event-service/src/main/java/app/sportahub/eventservice/config/kafka/KafkaProducerConfig.java
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/config/kafka/KafkaProducerConfig.java
@@ -31,11 +31,6 @@ public class KafkaProducerConfig {
     }
 
     @Bean
-    public KafkaTemplate<String, Object> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
-    }
-
-    @Bean
     public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
         return new KafkaTemplate<>(producerFactory);
     }
