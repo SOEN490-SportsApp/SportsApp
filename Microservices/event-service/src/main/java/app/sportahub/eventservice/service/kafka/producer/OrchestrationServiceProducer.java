bab6a80f075d4572a7625a520fc3cf9069f29ecd
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-04-02 17:44:57 -0400

    GH-382: [feat] Refactor OrchestrationServiceProducer to send NotificationEvent and remove outdated notification methods

--- Microservices/event-service/src/main/java/app/sportahub/eventservice/service/kafka/producer/OrchestrationServiceProducer.java
+++ Microservices/event-service/src/main/java/app/sportahub/eventservice/service/kafka/producer/OrchestrationServiceProducer.java
@@ -1,9 +1,10 @@
 package app.sportahub.eventservice.service.kafka.producer;
 
+import app.sportahub.kafka.events.notification.NotificationEvent;
+
 import java.util.List;
+import java.util.Map;
 
 public interface OrchestrationServiceProducer {
-    void sendEventUpdateNotifications(String eventId, String eventName, List<String> userIds, String updatedAt);
-    void sendEventCancellationNotifications(String eventId, String eventName, List<String> userIds, String reason, String cancelledBy);
-    void notifyParticipantsOfNewJoiner(String eventId, String eventName, List<String> previousParticipantIds, String newUserId);
+    void sendNotificationEvent(NotificationEvent event);
 }
