2a9b8c983692ac4e1f68cf20ee4f0fb1656aae8d
Walid <walidoo_2002@hotmail.com>
2025-04-01 20:09:29 -0400

    GH-382: [refactor] Consolidate notification consumption methods into a single method

--- Microservices/notification-service/src/main/java/app/sportahub/notificationservice/service/kafka/consumer/OrchestrationServiceConsumerImpl.java
+++ Microservices/notification-service/src/main/java/app/sportahub/notificationservice/service/kafka/consumer/OrchestrationServiceConsumerImpl.java
@@ -16,28 +16,8 @@ public class OrchestrationServiceConsumerImpl implements OrchestrationServiceCon
 
     @KafkaListener(topics = "event-updates", groupId = "notification-service", containerFactory = "kafkaListenerContainerFactory")
     @Override
-    public void consumeEventNotification(NotificationRequest request) {
-        log.info("Received event update notification: {}", request);
-        notificationService.processNotification(request);
-    }
-
-    @KafkaListener(topics = "friend-requests", groupId = "notification-service", containerFactory = "kafkaListenerContainerFactory")
-    @Override
-    public void consumeFriendRequestNotification(NotificationRequest request) {
-        log.info("Received friend request notification: {}", request);
-        notificationService.processNotification(request);
-    }
-
-    @KafkaListener(topics = "user-mentions", groupId = "notification-service", containerFactory = "kafkaListenerContainerFactory")
-    public void consumeUserMentionNotification(NotificationRequest request) {
-        log.info("Received user mention notification: {}", request);
-        notificationService.processNotification(request);
-    }
-
-    @KafkaListener(topics = "badge-assignments", groupId = "notification-service", containerFactory = "kafkaListenerContainerFactory")
-    @Override
-    public void consumeBadgeAssignmentNotification(NotificationRequest request) {
-        log.info("Received badge assignment notification: {}", request);
+    public void consumeNotification(NotificationRequest request) {
+        log.info("Received notification: {}", request);
         notificationService.processNotification(request);
     }
 
